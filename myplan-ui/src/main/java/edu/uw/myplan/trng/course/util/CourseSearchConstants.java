9051936f51cbfaac7cadf72bacbdd68fd646f3dd
kmuthusw <kmuthusw@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2011-11-04 20:46:51 +0000

    Added paged navigatin between search input and result display

---
+++ myplan-ui/src/main/java/edu/uw/myplan/trng/course/util/CourseSearchConstants.java
@@ -0,0 +1,15 @@
+package edu.uw.myplan.trng.course.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kmuthu
+ * Date: 11/4/11
+ * Time: 10:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CourseSearchConstants {
+
+    public static final String COURSE_SEARCH_PAGE = "course_search";
+
+    public static final String COURSE_SEARCH_RESULT_PAGE = "course_search_result";
+}
