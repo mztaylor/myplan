c79dea18e0dd30e78ccf0790de0e2c0072d5a88d
kmuthusw <kmuthusw@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2011-11-14 18:24:43 +0000

    initial search facet js

--- myplan-web/src/main/webapp/ks-myplan/scripts/course-search.js
+++ myplan-web/src/main/webapp/ks-myplan/scripts/course-search.js
@@ -2,3 +2,12 @@
 jq(document).ready(function() {
     jq("#course_search_fields_span span input[type='text']").blur();
 } );
+
+
+function facetFilter(colIndex, filterText ) {
+
+     oTable = jq('#course_search_results_datatable').dataTable();
+
+    /* Filter immediately */
+    oTable.fnFilter(filterText, colIndex );
+}
