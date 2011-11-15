12d173410af3e13c28777fc18f410f8f9705cd7c
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2011-11-15 00:22:48 +0000

    Tokenizes search query, distinquishing course number from course level from general terms. Also accepts multiple general terms, fires search per term, and merges the result.

---
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/edu/uw/myplan/trng/course/controller/QueryTokenizer.java
@@ -0,0 +1,83 @@
+package edu.uw.myplan.trng.course.controller;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class QueryTokenizer
+{
+	enum Rule
+	{
+        // Order significant. See matcher's foreach loop below.
+		LEVEL( "[0-9][Xx][Xx]" ),
+		NUMBER( "[0-9]+" ),
+		WORD( "[A-Za-z]+" ),
+		QUOTED( "\"[^\"]*\"" );
+		
+		public Pattern pattern;
+		
+		Rule( String regex )
+		{
+			pattern = Pattern.compile( regex );
+		}
+	}
+	
+	public static class Token
+	{
+		Rule rule;
+		String value;
+		public Token( Rule rule, String value )
+		{
+			this.rule = rule;
+			this.value = value;
+		}
+	}
+	
+	public List<Token> tokenize( String source )
+	{
+		ArrayList<Token> tokens = new ArrayList<Token>();
+		int pos = 0;
+		final int len = source.length();
+		
+		Matcher m = Pattern.compile( "dummy" ).matcher( source );
+		m.useTransparentBounds( true );
+		m.useAnchoringBounds( false );
+
+		loop:
+		while( pos < len )
+		{
+			m.region( pos, len );
+			for( Rule rule : Rule.values() )
+			{
+				if( m.usePattern( rule.pattern ).lookingAt() )
+				{
+					String value = source.substring( m.start(), m.end() );
+					Token token = new Token( rule, value );
+					tokens.add( token );	
+					pos = m.end();
+					continue loop;
+				}
+			}
+			
+			pos++;
+		}
+		
+		return tokens;
+	}
+
+
+	public static void main( String[] args ) 
+		throws Exception
+	{
+		String str = "abc123 xyzzy XYZZY 4xx 4XX \"quoted text\" 0 00 000 0000";
+		QueryTokenizer toho = new QueryTokenizer();
+		List<Token> result = toho.tokenize( str );
+		for( Token t : result )
+		{
+			System.out.println(t.value);
+		}
+
+        String argh = "\"abcdef\"";
+        argh = argh.substring( 1, argh.length() - 1 );
+        System.out.println( argh );
+	}
+}
