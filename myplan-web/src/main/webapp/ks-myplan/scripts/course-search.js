bbe1752afe32e6446630b8d2e3d3f7d4d22e3cd8
garettg <garettg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2011-11-16 00:13:40 +0000

    Updated JS for facets

--- myplan-web/src/main/webapp/ks-myplan/scripts/course-search.js
+++ myplan-web/src/main/webapp/ks-myplan/scripts/course-search.js
@@ -1,14 +1,46 @@
-/* Course Search */
+var arrFacets = [];
+
 jq(document).ready(function() {
     jq("#course_search_fields_span span input[type='text']").blur();
 } );
 
+function facetFilter(colIndex, filterText) {
 
-function facetFilter(colIndex, filterText ) {
+    if ( !arrFacets[colIndex] ) {
+    	arrFacets[colIndex] = [];
+    }
+    var key = jq.inArray(String(filterText), arrFacets[colIndex]);
+    if ( key === -1 ) {
+    	arrFacets[colIndex].push(String(filterText));
+    } else {
+    	arrFacets[colIndex].splice(key, 1);
+    }
 
-     oTable = jq('#course_search_results_datatable').dataTable();
+    filterDataTable(arrFacets);
+}
 
-    /* Filter immediately */
-    oTable.fnFilter(filterText, colIndex );
+function filterDataTable(arrFacets) {
+
+    oTable = jq('#course_search_results_datatable').dataTable();
+
+	for ( var i = 0; i < arrFacets.length; i++ ) {
+
+        oTable.fnFilter('', i, true, false);
+
+        var queryString;
+
+		if ( arrFacets[i] && arrFacets[i].length > 0 ) {
+
+            for ( var n = 0; n < arrFacets[i].length; n++ ) {
+
+				if ( n === 0 ) {
+					queryString = arrFacets[i][n];
+				} else {
+					queryString = queryString + '|' + arrFacets[i][n];
+				}
+			}
+            oTable.fnFilter(queryString, i, true, false);
+		}
+	}
 }
 
