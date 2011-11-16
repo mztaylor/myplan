a1f349d26ec9b4c280eb7093c596d521ddad77b6
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2011-11-16 02:00:25 +0000

    Restored Glenn and Kamal's facet code that I inadvertently wiped out. Oops.

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/edu/uw/myplan/trng/course/controller/CourseSearchController.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/edu/uw/myplan/trng/course/controller/CourseSearchController.java
@@ -19,12 +19,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.namespace.QName;
 
+import edu.uw.myplan.trng.course.util.*;
+import org.apache.log4j.Logger;
+
 import edu.uw.myplan.trng.course.dataobject.CourseSearchItem;
 import edu.uw.myplan.trng.course.form.CourseSearchForm;
-import edu.uw.myplan.trng.course.util.CourseSearchConstants;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.krad.web.controller.UifControllerBase;
 import org.kuali.rice.krad.web.form.UifFormBase;
+import org.kuali.student.common.exceptions.OperationFailedException;
 import org.kuali.student.common.search.dto.*;
 import org.kuali.student.core.atp.dto.AtpSeasonalTypeInfo;
 import org.kuali.student.core.atp.service.AtpService;
@@ -44,17 +47,24 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
 
 
 @Controller
 @RequestMapping(value = "/course")
 public class CourseSearchController extends UifControllerBase {
 
+    final Logger logger = Logger.getLogger(CourseSearchController.class);
+
     private transient LuService luService;
 
     private transient AtpService atpService;
 
     private transient CourseService courseService;
+
+    private transient Map<String, String> atpCache;
 
     @Override
     protected UifFormBase createInitialForm(HttpServletRequest request) {
@@ -70,6 +80,12 @@ public class CourseSearchController extends UifControllerBase {
     @RequestMapping(params = "methodToCall=searchForCourses")
     public ModelAndView searchForCourses(@ModelAttribute("KualiForm") CourseSearchForm courseSearchForm, BindingResult result,
                                          HttpServletRequest request, HttpServletResponse response) {
+        //  Initialize facets.
+        CurriculumFacet curriculumFacet = new CurriculumFacet();
+        CreditsFacet creditsFacet = new CreditsFacet();
+        CourseLevelFacet courseLevelFacet = new CourseLevelFacet();
+        GenEduReqFacet genEduReqFacet = new GenEduReqFacet();
+        TimeScheduleFacet timeScheduleFacet = new TimeScheduleFacet();
 
         String query = courseSearchForm.getSearchQuery();
         QueryTokenizer tokenizer = new QueryTokenizer();
@@ -151,8 +167,25 @@ public class CourseSearchController extends UifControllerBase {
                 item.setGenEduReq(formatGenEduReq(course));
                 item.setLevel(course.getLevel());
                 // item.setStatus(TBD);
+
+                //  Update facet info and code the item.
+                curriculumFacet.process(course, item);
+                courseLevelFacet.process(course, item);
+                genEduReqFacet.process(course, item);
+                creditsFacet.process(course, item);
+                timeScheduleFacet.process(course, item);
+
                 searchResults.add(item);
             }
+
+            //  Add the facet data to the response.
+            courseSearchForm.setCurriculumFacetItems(curriculumFacet.getFacetItems());
+            courseSearchForm.setCreditsFacetItems(creditsFacet.getFacetItems());
+            courseSearchForm.setGenEduReqFacetItems(genEduReqFacet.getFacetItems());
+            courseSearchForm.setCourseLevelFacetItems(courseLevelFacet.getFacetItems());
+            courseSearchForm.setTimeScheduleFacetItems(timeScheduleFacet.getFacetItems());
+
+            //  Add the search results to the response.
             courseSearchForm.setCourseSearchResults(searchResults);
 
         } catch (Exception e) {
@@ -168,12 +201,12 @@ public class CourseSearchController extends UifControllerBase {
 
         List<ResultComponentInfo> options = courseInfo.getCreditOptions();
         if (options.size() == 0) {
-            //  TODO: Log a warning
+            logger.warn("Credit options list was empty.");
             return credits;
         }
         /* At UW this list should only contain one item. */
         if (options.size() > 1) {
-            //  TODO: Log a warning.
+            logger.warn("Credit option list contained more than one value.");
         }
         ResultComponentInfo rci = options.get(0);
 
@@ -184,13 +217,14 @@ public class CourseSearchController extends UifControllerBase {
         String type = rci.getType();
         if (type.equals(CourseAssemblerConstants.COURSE_RESULT_COMP_TYPE_CREDIT_FIXED)) {
             credits = rci.getAttributes().get(CourseAssemblerConstants.COURSE_RESULT_COMP_ATTR_FIXED_CREDIT_VALUE);
+            credits = trimCredits(credits);
         } else if (type.equals(CourseAssemblerConstants.COURSE_RESULT_COMP_TYPE_CREDIT_MULTIPLE)) {
             StringBuilder cTmp = new StringBuilder();
             for (String c : rci.getResultValues()) {
                 if (cTmp.length() != 0) {
                     cTmp.append(" ,");
                 }
-                cTmp.append(c);
+                cTmp.append(trimCredits(c));
             }
             credits = cTmp.toString();
         } else if (type.equals(CourseAssemblerConstants.COURSE_RESULT_COMP_TYPE_CREDIT_VARIABLE)) {
@@ -201,26 +235,64 @@ public class CourseSearchController extends UifControllerBase {
         return credits;
     }
 
+    /**
+     *  Drop the decimal point and and trailing zero from credits.
+     * @return The supplied value minus the trailing ".0"
+     */
+    private String trimCredits(String credits) {
+        if (credits.endsWith(".0")) {
+            credits = credits.substring(0, credits.indexOf("."));
+        }
+        return credits;
+    }
+
     private String formatScheduledTime(CourseInfo courseInfo) {
         List<String> terms = courseInfo.getTermsOffered();
+        Collections.sort(terms);
         StringBuilder termsTmp = new StringBuilder();
         for (String term : terms) {
             if (termsTmp.length() != 0) {
                 termsTmp.append(", ");
             }
-
-            AtpSeasonalTypeInfo atpSto;
-            try {
-                atpSto = getAtpService().getAtpSeasonalType(term);
-            } catch (Exception e) {
-                // TODO: Shouldn't catch the kitchen sink here.
-                e.printStackTrace();
-                termsTmp.append("?");
-                continue;
-            }
-            termsTmp.append(atpSto.getName().substring(0,2).toUpperCase());
+            termsTmp.append(getTerm(term));
         }
         return termsTmp.toString();
+    }
+
+    /**
+     * Gets a "term" from the AtpSeasonalTypes cache.
+     * @param term The key of the AtpSeasonalTypeInfo
+     * @return A String representation of the AtpSeasonalTypeInfo.
+     */
+    private String getTerm(String term) {
+        if (atpCache == null) {
+            initializeAtpSeasonalTypesCache();
+        }
+        String t = atpCache.get(term);
+        if (t == null) {
+            logger.error("Term [" + term + "] was not found in the ATP cache. Attempting to re-initialize the the cache");
+            initializeAtpSeasonalTypesCache();
+            t = "?";
+        }
+        return t;
+    }
+
+    /**
+     * Initializes ATP term cache.
+     * AtpSeasonalTypes rarely change, so fetch them all and store them in a Map.
+     */
+    private void initializeAtpSeasonalTypesCache() {
+        atpCache = new HashMap<String, String>();
+        List<AtpSeasonalTypeInfo> atpSeasonalTypeInfos;
+        try {
+            atpSeasonalTypeInfos = getAtpService().getAtpSeasonalTypes();
+        } catch (OperationFailedException e) {
+            logger.error("ATP seasonal types lookup failed.", e);
+            return;
+        }
+        for (AtpSeasonalTypeInfo ti : atpSeasonalTypeInfos) {
+            atpCache.put(ti.getId(), ti.getName().substring(0,2).toUpperCase());
+        }
     }
 
     private String formatGenEduReq(CourseInfo courseInfo) {
@@ -252,5 +324,4 @@ public class CourseSearchController extends UifControllerBase {
         }
         return this.courseService;
     }
-
 }
