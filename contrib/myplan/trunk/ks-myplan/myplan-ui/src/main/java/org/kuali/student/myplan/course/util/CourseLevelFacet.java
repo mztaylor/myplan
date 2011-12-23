a0ec5b203a60a274bce14629b4e522289ff31762
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2011-12-23 22:52:28 +0000

    Unit tests for Credits, CourseLevel facets.

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/CourseLevelFacet.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/CourseLevelFacet.java
@@ -3,13 +3,41 @@ package org.kuali.student.myplan.course.util;
 import org.kuali.student.myplan.course.dataobject.CourseSearchItem;
 import org.kuali.student.myplan.course.dataobject.FacetItem;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 /**
 *  Logic for building list of Course Level FacetItems and coding CourseSearchItems.
 */
 public class CourseLevelFacet extends AbstractFacet {
 
+    private HashSet<Integer> courseFacetSet = new HashSet<Integer>();
+
     public CourseLevelFacet() {
         super();
+    }
+
+    /**
+     * Overriding because the course level facet list needs to be in numeric order rather than string order.
+     *
+     * @return A list of FacetItems.
+     */
+    @Override
+    public List<FacetItem> getFacetItems() {
+        Integer[] list = courseFacetSet.toArray( new Integer[0] );
+        Arrays.sort(list);
+
+        for( Integer credit : list ) {
+            FacetItem item = new FacetItem();
+            String display = credit.toString();
+            String key = FACET_KEY_DELIMITER + display + FACET_KEY_DELIMITER;
+            item.setKey( key );
+            item.setDisplayName( display );
+            facetItems.add( item );
+        }
+
+        return facetItems;
     }
 
     /**
@@ -17,27 +45,19 @@ public class CourseLevelFacet extends AbstractFacet {
      */
     @Override
     public void process(CourseSearchItem course) {
-        String displayName = course.getLevel();
-        String key = null;
-
-        if (displayName == null || displayName.equals("")) {
-            displayName = unknownFacetDisplayName;
-            key = unknownFacetKey;
-        } else {
-            key = displayName;
-        }
+        String key = course.getLevel();
+        int level = Integer.valueOf( key );
+        courseFacetSet.add( level );
 
         key = FACET_KEY_DELIMITER + key + FACET_KEY_DELIMITER;
 
-        //  If it's a new facet key then create a new FacetItem.
-        if (isNewFacetKey( key )) {
-            FacetItem fItem = new FacetItem();
-            //  The display name and the key are the same in this case.
-            fItem.setKey(key);
-            fItem.setDisplayName(displayName);
-            facetItems.add(fItem);
-        }
         //  Code the item with the facet key.
         course.setCourseLevelFacetKey(key);
+    }
+
+    public static void main( String[] args )
+    {
+        int level = Integer.valueOf( "500" );
+
     }
 }
