3eaf116b9dbfedc690c69d222d65b4499ca018d8
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-01-09 17:26:23 +0000

    Saved Courses Inquiry View initial effort.

---
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/dataobject/SavedCoursesItem.java
@@ -0,0 +1,80 @@
+package org.kuali.student.myplan.course.dataobject;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class SavedCoursesItem {
+
+    private String courseID;
+    private String code;
+    private String title;
+    private String credit;
+    private List<String> scheduleList = new ArrayList<String>();
+    private List<String> prereqList = new ArrayList<String>();
+    private Date added;
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID( String courseID ) {
+        this.courseID = courseID;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode( String code ) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle( String title ) {
+        this.title = title;
+    }
+
+    public String getCredit() {
+        return credit;
+    }
+
+    public void setCredit( String credit ) {
+        this.credit = credit;
+    }
+
+    public List<String> getScheduleList() {
+        return this.scheduleList;
+    }
+
+    public void setScheduleList(List<String> scheduleList) {
+        this.scheduleList = scheduleList;
+    }
+
+    public void addSchedule( String term ) {
+        scheduleList.add( term );
+    }
+
+    public List<String> getPrereqList() {
+        return prereqList;
+    }
+
+    public void setPrereqList( List<String> prereqList ) {
+        this.prereqList = prereqList;
+    }
+
+    public void addPrereq( String prereq ) {
+        prereqList.add( prereq );
+    }
+
+    public Date getAdded() {
+        return added;
+    }
+
+    public void setAdded( Date added ) {
+        this.added = added;
+    }
+}
