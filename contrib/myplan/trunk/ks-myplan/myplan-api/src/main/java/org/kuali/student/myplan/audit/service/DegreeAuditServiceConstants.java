28873ce63c67aa856dea86936b5260526ecd60a1
kmuthusw <kmuthusw@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-02-17 17:28:39 +0000

    Adding type constants for DA

--- contrib/myplan/trunk/ks-myplan/myplan-api/src/main/java/org/kuali/student/myplan/audit/service/DegreeAuditServiceConstants.java
+++ contrib/myplan/trunk/ks-myplan/myplan-api/src/main/java/org/kuali/student/myplan/audit/service/DegreeAuditServiceConstants.java
@@ -11,4 +11,9 @@ import org.kuali.student.r2.common.constants.CommonServiceConstants;
 public class DegreeAuditServiceConstants {
     public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "da";
     public static final String SERVICE_NAME = "DegreeAuditService";
+
+
+    public static final String AUDIT_TYPE_KEY_DEFAULT = "kauli.audit.type.default";
+    public static final String AUDIT_TYPE_KEY_HTML = "kauli.audit.type.html";
+    public static final String AUDIT_TYPE_KEY_PRINT = "kauli.audit.type.print";
 }
