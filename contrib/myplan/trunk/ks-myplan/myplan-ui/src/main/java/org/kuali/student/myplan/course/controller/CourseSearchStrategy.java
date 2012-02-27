afefa3f282d05e0b1ff617ccf91548d72d728800
kmuthusw <kmuthusw@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-02-27 23:21:14 +0000

    Added enum service getter and setter to CourseSearchStrategy

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/controller/CourseSearchStrategy.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/controller/CourseSearchStrategy.java
@@ -16,12 +16,13 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
-public class CourseSearchStrategy
-{
+public class CourseSearchStrategy {
     private final Logger logger = Logger.getLogger(CourseSearchStrategy.class);
 
     private transient EnumerationManagementService enumService;
-    
+
+    private transient LuService luService;
+
 
     protected synchronized EnumerationManagementService getEnumerationService() {
         if (this.enumService == null) {
@@ -30,30 +31,24 @@ public class CourseSearchStrategy
         }
         return this.enumService;
     }
-    
-    public HashMap<String,String> fetchCourseDivisions()
-    {
-        HashMap<String,String> map = new HashMap<String,String>();
-        try
-        {
-            SearchRequest request = new SearchRequest( "myplan.distinct.clu.divisions" );
 
-            SearchResult result = getLuService().search( request );
+    public HashMap<String, String> fetchCourseDivisions() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        try {
+            SearchRequest request = new SearchRequest("myplan.distinct.clu.divisions");
 
-            for( SearchResultRow row : result.getRows() )
-            {
-                for( SearchResultCell cell : row.getCells() )
-                {
+            SearchResult result = getLuService().search(request);
+
+            for (SearchResultRow row : result.getRows()) {
+                for (SearchResultCell cell : row.getCells()) {
                     String division = cell.getValue();
                     // Store both trimmed and original, because source data
                     // is sometimes space padded.
-                    String key = division.trim().replaceAll( "\\s+", "" );
-                    map.put( key, division );
+                    String key = division.trim().replaceAll("\\s+", "");
+                    map.put(key, division);
                 }
             }
-        }
-        catch( Exception e )
-        {
+        } catch (Exception e) {
             // TODO: Handle this exception better
             e.printStackTrace();
         }
@@ -65,86 +60,71 @@ public class CourseSearchStrategy
     public final static String NO_CAMPUS = "-1";
 
 
-    public void addCampusParams( ArrayList<SearchRequest> requests, CourseSearchForm form )
-    {
+    public void addCampusParams(ArrayList<SearchRequest> requests, CourseSearchForm form) {
         String str = form.getCampusSelect();
-        String[] results=null;
-                if(!str.equalsIgnoreCase(""))
-                {
-                    results = str.split(",");
-                }
+        String[] results = null;
+        if (!str.equalsIgnoreCase("")) {
+            results = str.split(",");
+        }
 
-        List<EnumeratedValueInfo> enumeratedValueInfoList =null;
-                try{
+        List<EnumeratedValueInfo> enumeratedValueInfoList = null;
+        try {
 
-                    enumeratedValueInfoList = getEnumerationService().getEnumeratedValues("kuali.lu.campusLocation", null, null, null);
-                }
-                catch (Exception e)
-                {
-                    logger.error("No Values for campuses found",e);
-                }
-        String[] campus = new String[enumeratedValueInfoList.size()-1];
-                for(int k=0;k<campus.length;k++)
-                {
-                    campus[k]=NO_CAMPUS;
-                }
-        if(results!=null){
-                for(int i=0;i<results.length;i++)
-                {
-                    for(EnumeratedValueInfo enumeratedValueInfo: enumeratedValueInfoList)
-                    {
-                        if(results[i].equalsIgnoreCase(enumeratedValueInfo.getCode()))
-                        {
-                            campus[i]=results[i];
-                            break;
-                        }
+            enumeratedValueInfoList = getEnumerationService().getEnumeratedValues("kuali.lu.campusLocation", null, null, null);
+        } catch (Exception e) {
+            logger.error("No Values for campuses found", e);
+        }
+        String[] campus = new String[enumeratedValueInfoList.size() - 1];
+        for (int k = 0; k < campus.length; k++) {
+            campus[k] = NO_CAMPUS;
+        }
+        if (results != null) {
+            for (int i = 0; i < results.length; i++) {
+                for (EnumeratedValueInfo enumeratedValueInfo : enumeratedValueInfoList) {
+                    if (results[i].equalsIgnoreCase(enumeratedValueInfo.getCode())) {
+                        campus[i] = results[i];
+                        break;
                     }
                 }
-         }
-        //  Add the individual term items.
-            for( SearchRequest request : requests )
-            {
-                for(int j=0;j<campus.length;j++)
-                {
-                    int count=j+1;
-                    String campusKey="campus"+count;
-                    request.addParam(campusKey,campus[j]);
-                }
             }
+        }
+        //  Add the individual term items.
+        for (SearchRequest request : requests) {
+            for (int j = 0; j < campus.length; j++) {
+                int count = j + 1;
+                String campusKey = "campus" + count;
+                request.addParam(campusKey, campus[j]);
+            }
+        }
     }
 
     /**
-     *
      * @param divisionMap for reference
-     * @param query initial query
-     * @param divisions matches found
+     * @param query       initial query
+     * @param divisions   matches found
      * @return query string, minus matches found
      */
-    public String extractDivisions( HashMap<String,String> divisionMap, String query, List<String> divisions )
-    {
+    public String extractDivisions(HashMap<String, String> divisionMap, String query, List<String> divisions) {
         boolean match = true;
-        while( match )
-        {
+        while (match) {
             match = false;
             // Retokenize after each division found is removed
             // Remove extra spaces to normalize input
-            query = query.trim().replaceAll( "\\s+", " " );
-            List<QueryTokenizer.Token> tokens = QueryTokenizer.tokenize( query );
+            query = query.trim().replaceAll("\\s+", " ");
+            List<QueryTokenizer.Token> tokens = QueryTokenizer.tokenize(query);
             List<String> list = QueryTokenizer.toStringList(tokens);
-            List<String> pairs = TokenPairs.toPairs( list );
-            TokenPairs.sortedLongestFirst( pairs );
+            List<String> pairs = TokenPairs.toPairs(list);
+            TokenPairs.sortedLongestFirst(pairs);
 
             Iterator<String> i = pairs.iterator();
-            while( match == false && i.hasNext() )
-            {
+            while (match == false && i.hasNext()) {
                 String pair = i.next();
 
-                String key = pair.replace( " ", "" );
-                if( divisionMap.containsKey( key ))
-                {
-                    String division = divisionMap.get( key );
-                    divisions.add( division );
-                    query = query.replace( pair, "" );
+                String key = pair.replace(" ", "");
+                if (divisionMap.containsKey(key)) {
+                    String division = divisionMap.get(key);
+                    divisions.add(division);
+                    query = query.replace(pair, "");
                     match = true;
                 }
             }
@@ -153,85 +133,76 @@ public class CourseSearchStrategy
     }
 
     public void addDivisionSearches(List<String> divisions, List<String> codes, List<String> levels, List<SearchRequest> requests) {
-        for( String division : divisions )
-        {
+        for (String division : divisions) {
             boolean needDivisionQuery = true;
 
-            for( String code : codes )
-            {
+            for (String code : codes) {
                 needDivisionQuery = false;
-                SearchRequest request = new SearchRequest( "myplan.lu.search.divisionAndCode" );
-                request.addParam( "division", division );
-                request.addParam( "code", code );
-                requests.add( request );
+                SearchRequest request = new SearchRequest("myplan.lu.search.divisionAndCode");
+                request.addParam("division", division);
+                request.addParam("code", code);
+                requests.add(request);
             }
 
-            for( String level : levels )
-            {
+            for (String level : levels) {
                 needDivisionQuery = false;
 
                 // Converts "1XX" to "100"
-                level = level.substring( 0, 1 ) + "00";
+                level = level.substring(0, 1) + "00";
 
-                SearchRequest request = new SearchRequest( "myplan.lu.search.divisionAndLevel" );
+                SearchRequest request = new SearchRequest("myplan.lu.search.divisionAndLevel");
                 request.addParam("division", division);
                 request.addParam("level", level);
-                requests.add( request );
+                requests.add(request);
             }
 
-            if( needDivisionQuery )
-            {
-                SearchRequest request = new SearchRequest( "myplan.lu.search.division" );
+            if (needDivisionQuery) {
+                SearchRequest request = new SearchRequest("myplan.lu.search.division");
                 request.addParam("division", division);
-                requests.add( request );
+                requests.add(request);
             }
         }
     }
 
     public void addFullTextSearches(String query, List<SearchRequest> requests) {
-        List<QueryTokenizer.Token> tokens = QueryTokenizer.tokenize( query );
+        List<QueryTokenizer.Token> tokens = QueryTokenizer.tokenize(query);
 
-        for( QueryTokenizer.Token token : tokens )
-        {
+        for (QueryTokenizer.Token token : tokens) {
             String queryText = null;
-            switch( token.rule )
-            {
+            switch (token.rule) {
                 case WORD:
                     queryText = token.value;
                     break;
                 case QUOTED:
                     queryText = token.value;
-                    queryText = queryText.substring( 1, queryText.length() - 1 );
+                    queryText = queryText.substring(1, queryText.length() - 1);
                     break;
                 default:
                     break;
             }
-            SearchRequest request = new SearchRequest( "myplan.lu.search.fulltext" );
+            SearchRequest request = new SearchRequest("myplan.lu.search.fulltext");
             request.addParam("queryText", queryText);
-            requests.add( request );
+            requests.add(request);
         }
     }
 
-    public List<SearchRequest> queryToRequests( CourseSearchForm form )
-        throws Exception
-    {
+    public List<SearchRequest> queryToRequests(CourseSearchForm form)
+            throws Exception {
         String query = form.getSearchQuery().toUpperCase();
 
         List<String> levels = QueryTokenizer.extractCourseLevels(query);
-        for( String level : levels )
-        {
-            query = query.replace( level, "" );
+        for (String level : levels) {
+            query = query.replace(level, "");
         }
         List<String> codes = QueryTokenizer.extractCourseCodes(query);
-        for( String code : codes )
-        {
-            query = query.replace( code, "" );
+        for (String code : codes) {
+            query = query.replace(code, "");
         }
 
-        HashMap<String,String> divisionMap = fetchCourseDivisions();
+        HashMap<String, String> divisionMap = fetchCourseDivisions();
 
         ArrayList<String> divisions = new ArrayList<String>();
-        query = extractDivisions( divisionMap, query, divisions );
+        query = extractDivisions(divisionMap, query, divisions);
 
 
         ArrayList<SearchRequest> requests = new ArrayList<SearchRequest>();
@@ -241,12 +212,12 @@ public class CourseSearchStrategy
 
         addFullTextSearches(query, requests);
 
-        addCampusParams( requests, form );
+        addCampusParams(requests, form);
 
         return requests;
     }
 
-    private transient LuService luService;
+
 
     //Note: here I am using r1 LuService implementation!!!
     protected LuService getLuService() {
@@ -258,5 +229,13 @@ public class CourseSearchStrategy
 
     public void setLuService(LuService luService) {
         this.luService = luService;
+    }
+
+    public EnumerationManagementService getEnumService() {
+        return enumService;
+    }
+
+    public void setEnumService(EnumerationManagementService enumService) {
+        this.enumService = enumService;
     }
 }
