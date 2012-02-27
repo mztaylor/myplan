401a8089d3e6fe1c80217a5917cd6881a01227ce
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-02-27 18:35:03 +0000

    Added method to get the title for the course prefix record from the enumerationmanagementservice

--- myplan-ui/src/main/java/org/kuali/student/myplan/course/util/CurriculumFacet.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/course/util/CurriculumFacet.java
@@ -2,7 +2,14 @@ package org.kuali.student.myplan.course.util;
 
 import javax.xml.namespace.QName;
 
+import org.apache.log4j.Logger;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.student.common.exceptions.DoesNotExistException;
+import org.kuali.student.common.exceptions.InvalidParameterException;
+import org.kuali.student.common.exceptions.MissingParameterException;
+import org.kuali.student.common.exceptions.OperationFailedException;
+import org.kuali.student.core.enumerationmanagement.dto.EnumeratedValueInfo;
+import org.kuali.student.core.enumerationmanagement.service.EnumerationManagementService;
 import org.kuali.student.core.organization.service.OrganizationService;
 import org.kuali.student.myplan.course.dataobject.CourseSearchItem;
 import org.kuali.student.myplan.course.dataobject.FacetItem;
@@ -14,6 +21,17 @@ import java.util.*;
  */
 public class CurriculumFacet extends AbstractFacet {
 
+    private final Logger logger = Logger.getLogger(CurriculumFacet.class);
+
+    private transient EnumerationManagementService enumService;
+
+    protected synchronized EnumerationManagementService getEnumerationService() {
+        if (this.enumService == null) {
+            this.enumService = (EnumerationManagementService) GlobalResourceLoader
+                    .getService(new QName(CourseSearchConstants.ENUM_SERVICE_NAMESPACE, "EnumerationManagementService"));
+        }
+        return this.enumService;
+    }
     public CurriculumFacet() {
         super();
     }
@@ -25,11 +43,18 @@ public class CurriculumFacet extends AbstractFacet {
         String[] list = curriculumFacetSet.toArray( new String[0] );
         Arrays.sort( list );
 
+
+
         for( String display : list ) {
             FacetItem item = new FacetItem();
+                       
             String key = FACET_KEY_DELIMITER + display + FACET_KEY_DELIMITER;
             item.setKey( key );
-            item.setDisplayName( display );
+            item.setDisplayName(display);
+            String title= this.getTitle(display);
+            if(title != null || title!= ""){
+            item.setTitle(title);
+            }
             facetItems.add( item );
         }
 
@@ -59,4 +84,32 @@ public class CurriculumFacet extends AbstractFacet {
         keys.add(key);
         course.setCurriculumFacetKeys(keys);
     }
+
+    /**
+     * To get the title for the respective display name
+     * @param display
+     * @return
+     */
+    protected String getTitle(String display) {
+             String titleValue=null;
+              try {
+                List<EnumeratedValueInfo> enumeratedValueInfoList = getEnumerationService().getEnumeratedValues("kuali.lu.subjectArea", null, null, null);
+                for(EnumeratedValueInfo enumVal : enumeratedValueInfoList)
+                {
+                    String code= enumVal.getCode().trim();
+                    if(code.equalsIgnoreCase(display))
+                    {
+                        titleValue=enumVal.getValue().trim();
+                        break;
+                    }
+                }
+
+            } catch (Exception e) {
+                logger.error("Could not load title value");
+            }
+        
+        return titleValue;
+    }
+    
+    
 }
