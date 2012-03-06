91fa854096a1d62966b107ac9af36832463593da
garettg <garettg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-03-06 22:27:29 +0000

    Fixed js

--- myplan-web/src/main/webapp/ks-myplan/scripts/audit.js
+++ myplan-web/src/main/webapp/ks-myplan/scripts/audit.js
@@ -1,6 +1,21 @@
 jq(document).ready(function(){
+    jq(".requirement .requirement-heading > div").each(function() {
+    	var status = jq(this).attr("class");
+    	switch (status) {
+    		case "complete":
+
+    			break;
+    		default:
+    			jq(this).parent(".requirement-heading").addClass("expanded");
+    	}
+    });
     jq(".requirement .requirement-heading").click(function(){
-        jq(this).toggleClass("collapsed");
-        jq(this).next(".requirement-body").slideToggle("slow");
+        if ( jq(this).hasClass("expanded") ) {
+        		jq(this).removeClass("expanded").addClass("collapsed");
+        		jq(this).next(".requirement-body").slideUp("slow");
+		} else {
+        		jq(this).removeClass("collapsed").addClass("expanded");
+        		jq(this).next(".requirement-body").slideDown("slow");
+        }
     });
 });
