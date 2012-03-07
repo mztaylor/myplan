b6b439e3c7c938d271844ac1e02641b347ebed7d
garettg <garettg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-03-07 23:47:19 +0000

    Updates to degree audit UI

--- myplan-web/src/main/webapp/ks-myplan/scripts/audit.js
+++ myplan-web/src/main/webapp/ks-myplan/scripts/audit.js
@@ -1,9 +1,17 @@
 jq(document).ready(function(){
+    //var count = 0;
     jq(".requirement .requirement-heading > div").each(function() {
+    	/*
+    	if (count == 3) {
+    		jq(this).attr("class","C");
+    	}
+    	++count;
+    	*/
     	var status = jq(this).attr("class");
     	switch (status) {
-    		case "complete":
-
+    		case "C":
+                jq(this).parent(".requirement-heading").addClass("collapsed");
+                jq(this).parent(".requirement-heading").next(".requirement-body").hide();
     			break;
     		default:
     			jq(this).parent(".requirement-heading").addClass("expanded");
@@ -17,5 +25,27 @@ jq(document).ready(function(){
         		jq(this).removeClass("collapsed").addClass("expanded");
         		jq(this).next(".requirement-body").slideDown("slow");
         }
+    });
+    jq("#requirement_status").change(function() {
+        var status = jq(this).val();
+        jq(".requirement").each(function() {
+			if ( status =='all' ) {
+				jq(this).show();
+			} else {
+				jq(this).filter( "."+status ).show();
+				jq(this).not( "."+status ).hide();
+			}
+        });
+    });
+    jq("a.jump").click(function(e) {
+    	e.stopPropagation();
+    	//console.log( jq(this).parents(".requirement-group").next(".requirement-group").offset() );
+    	var jump = jq(this).parents(".requirement-group").next(".requirement-group").offset();
+    	if ( jump != null ) {
+    		window.scrollTo(jump.left,jump.top);
+    	} else {
+    		window.scrollTo(0,0);
+    	}
+    	return false;
     });
 });
