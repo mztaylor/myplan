b8e269118adb06ebe00477139a52a255dd44a68b
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-03-16 19:35:55 +0000

    Unit testing

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/test/java/org/kuali/student/myplan/course/util/CurriculumFacetTest.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/test/java/org/kuali/student/myplan/course/util/CurriculumFacetTest.java
@@ -3,7 +3,10 @@ package org.kuali.student.myplan.course.util;
 import org.junit.Test;
 import org.kuali.student.myplan.course.dataobject.CourseSearchItem;
 import org.kuali.student.myplan.course.dataobject.FacetItem;
+import org.kuali.student.core.enumerationmanagement.dto.EnumeratedValueInfo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
@@ -11,25 +14,38 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
+
 public class CurriculumFacetTest {
 
     @Test
     public void testGetFacetItems() throws Exception {
         CurriculumFacet facet = new CurriculumFacet();
         CourseSearchItem course1 = new CourseSearchItem();
-        course1.setSubject("ABC");
+        course1.setSubject("A S");
         facet.process( course1 );
         CourseSearchItem course2 = new CourseSearchItem();
         course2.setSubject("XYZ");
         facet.process( course2 );
-
+        HashMap<String,List<EnumeratedValueInfo>> hashMap=new HashMap<String, List<EnumeratedValueInfo>>();
+        List<EnumeratedValueInfo> enumeratedValueInfoList=new ArrayList<EnumeratedValueInfo>();
+        EnumeratedValueInfo enumeratedValueInfo=new EnumeratedValueInfo();
+        enumeratedValueInfo.setCode("A S   ");
+        enumeratedValueInfo.setAbbrevValue("A S   ");
+        enumeratedValueInfo.setValue("AEROSPACE STUDIES (AIR FORCE ROTC)      ");
+        enumeratedValueInfo.setEnumerationKey("kuali.lu.subjectArea");
+        enumeratedValueInfoList.add(enumeratedValueInfo);
+        hashMap.put("kuali.lu.subjectArea",enumeratedValueInfoList);
+        facet.setHashMap(hashMap);
         List<FacetItem> list = facet.getFacetItems();
 
         assertTrue( list.size() == 2 );
-        assertEquals( list.get( 0 ).getDisplayName(), "ABC" );
-        assertEquals( list.get( 0 ).getKey(), ";ABC;" );
+        assertEquals( list.get( 0 ).getDisplayName(), "A S" );
+        assertEquals( list.get( 0 ).getKey(), ";A S;" );
+        assertEquals(list.get(0).getTitle(),"AEROSPACE STUDIES (AIR FORCE ROTC)");
         assertEquals( list.get( 1 ).getDisplayName(), "XYZ" );
         assertEquals( list.get( 1 ).getKey(), ";XYZ;" );
+        assertEquals( list.get( 1 ).getTitle(), null );
+
     }
 
     @Test
