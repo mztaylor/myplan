767e3a320a0859a5849e0114dc6041342512b61e
sudduth <sudduth@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-03-28 21:39:11 +0000

    Added ATP Ids.

--- myplan-ui/src/main/java/org/kuali/student/myplan/plan/dataobject/PlanItemDataObject.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/plan/dataobject/PlanItemDataObject.java
@@ -3,12 +3,19 @@ package org.kuali.student.myplan.plan.dataobject;
 import org.kuali.student.myplan.course.dataobject.CourseDetails;
 
 import java.util.Date;
+import java.util.List;
 
 public class PlanItemDataObject implements Comparable {
 
+    //  Common properties.
     private String id;
     private CourseDetails courseDetails;
     private Date dateAdded;
+
+    //  Planned course specific properties.
+
+    //  Ids of the ATPs associated with this course.
+    private List<String> atpIds;
 
     public String getId() {
         return id;
@@ -38,5 +45,13 @@ public class PlanItemDataObject implements Comparable {
     public int compareTo( Object object ) {
         PlanItemDataObject that = (PlanItemDataObject) object;
         return this.getDateAdded().compareTo( that.getDateAdded() ) * -1;
+    }
+
+    public List<String> getAtpIds() {
+        return atpIds;
+    }
+
+    public void setAtpIds(List<String> atpIds) {
+        this.atpIds = atpIds;
     }
 }
