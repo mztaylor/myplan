9871aeeb2fb9eb5a14f67940a56bb9cb1b7b7407
sudduth <sudduth@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-04-16 19:43:20 +0000

    ks-myplan/pom.xml: Added version number for jackson package (for JSON rendering in PlanController). PlanConstants.java: Added enum for javascript event names. Rearranged key names for feedback messages. PlanController.java: New PlanForm properties are now set on all plan item operations. PlanForm.java: Added properties for request status and javascript events (which will be throw by the UI to update "status" of plan items). CourseDetailsUI.xml, PlanItemsUI.xml: Put in new form view to render all "panels" needed for crud actions on plan items. KSMyPlan-ApplicationResources.properties: Made key names for feedback messages a little better.

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/PlanConstants.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/PlanConstants.java
@@ -19,19 +19,29 @@ public class PlanConstants extends AcademicPlanServiceConstants {
 
     public static final String COURSE_TYPE = LUConstants.CLU_TYPE_CREDIT_COURSE;
 
-    public static final String ERROR_KEY_OPERATION_FAILED = "error.myplan.savedCoursesList.operationFailed";
+    public static final String SUCCESS_KEY = "myplan.text.success";
 
-    public static final String ERROR_KEY_DATA_VALIDATION_ERROR = "error.myplan.savedCoursesList.dataValidationError";
-    public static final String ERROR_KEY_DUPLICATE_PLAN = "error.myplan.savedCoursesList.duplicatePlan";
-    public static final String ERROR_KEY_DUPLICATE_PLAN_ITEM = "error.myplan.savedCoursesList.duplicatePlanItem";
-    public static final String ERROR_KEY_UNKNOWN_PLAN_ITEM = "error.myplan.savedCoursesList.unknownPlanItem";
-    public static final String ERROR_KEY_INVALID_PARAM = "error.myplan.savedCoursesList.invalidParameter";
-    public static final String ERROR_KEY_MISSING_PARAM = "error.myplan.savedCoursesList.missingParameter";
-    public static final String ERROR_KEY_PERMISSION_DENIED = "error.myplan.savedCoursesList.permissionDenied";
+    public static final String ERROR_KEY_OPERATION_FAILED = "myplan.text.error.operationFailed";
+    public static final String ERROR_KEY_UNKNOWN_PLAN_ITEM = "myplan.text.error.savedCoursesList.unknownPlanItem";
+    public static final String ERROR_KEY_DATA_VALIDATION_ERROR = "myplan.text.error.dataValidationError";
+    public static final String ERROR_KEY_DUPLICATE_PLAN = "myplan.text.error.savedCoursesList.duplicatePlan";
+    public static final String ERROR_KEY_DUPLICATE_PLAN_ITEM = "myplan.text.error.savedCoursesList.duplicatePlanItem";
+    public static final String ERROR_KEY_INVALID_PARAM = "myplan.text.error.invalidParameter";
+    public static final String ERROR_KEY_MISSING_PARAM = "myplan.text.error.missingParameter";
+    public static final String ERROR_KEY_PERMISSION_DENIED = "myplan.text.error.permissionDenied";
 
     //  Global context info for use in service methods which need caching, but don't use the context argument.
     public static final ContextInfo CONTEXT_INFO = new ContextInfo();
 
     public static final String OTHER_TERM_KEY = "other";
     public static final String TERM_ID_PREFIX = "kuali.uw.atp.";
+
+    /**
+     * Names of javascript events that can be scheduled in response to the outcome of a plan item request.
+     */
+    public static enum JS_EVENT_NAME {
+        PLAN_ITEM_ADDED,
+        PLAN_ITEM_DELETED,
+        UPDATE_TOTAL_CREDITS
+    }
 }
