6dd3a976d56d9d40ebcb0b78411382fddd32d32a
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-04-17 17:22:17 +0000

    Added to support the view of Full Academic Plan

---
+++ myplan-ui/src/main/java/org/kuali/student/myplan/plan/dataobject/FullPlanTermItemsDataObject.java
@@ -0,0 +1,45 @@
+package org.kuali.student.myplan.plan.dataobject;
+
+import org.kuali.student.myplan.course.dataobject.CourseDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by IntelliJ IDEA.
+ * User: hemanthg
+ * Date: 4/13/12
+ * Time: 1:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FullPlanTermItemsDataObject {
+    private String term;
+    private int totalCredits;
+    private List<PlanItemDataObject> planItemDataObjects=new ArrayList<PlanItemDataObject>();
+
+
+
+
+    public List<PlanItemDataObject> getPlanItemDataObjects() {
+        return planItemDataObjects;
+    }
+
+    public void setPlanItemDataObjects(List<PlanItemDataObject> planItemDataObjects) {
+        this.planItemDataObjects = planItemDataObjects;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public int getTotalCredits() {
+        return totalCredits;
+    }
+
+    public void setTotalCredits(int totalCredits) {
+        this.totalCredits = totalCredits;
+    }
+}
