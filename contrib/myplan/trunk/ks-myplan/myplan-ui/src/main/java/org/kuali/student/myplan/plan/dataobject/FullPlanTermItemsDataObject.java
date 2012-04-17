9b32cef5f6d57b51a5a3c2952a2bc3ed9bf80469
sudduth <sudduth@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-04-17 22:15:54 +0000

    CourseDetails.java: Added property for inPlannedCourses. CourseDetailsInquiryViewHelperServiceImpl.java: Fixed inSavedCourses logic. Now sets inPlannedCourses flag. PlanItemsUI.xml: Added logic for contextual buttons based on course details status.

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/plan/dataobject/FullPlanTermItemsDataObject.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/plan/dataobject/FullPlanTermItemsDataObject.java
@@ -14,10 +14,7 @@ import java.util.List;
 public class FullPlanTermItemsDataObject {
     private String term;
     private int totalCredits;
-    private List<PlanItemDataObject> planItemDataObjects=new ArrayList<PlanItemDataObject>();
-
-
-
+    private List<PlanItemDataObject> planItemDataObjects = new ArrayList<PlanItemDataObject>();
 
     public List<PlanItemDataObject> getPlanItemDataObjects() {
         return planItemDataObjects;
