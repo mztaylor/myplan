5fd2e7466e64463bc57cb181556ce6d10748ccf2
garettg <garettg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-05-03 20:09:40 +0000

    Moved javascript logic to PlanItemsUI. Added event function to restore add button to search results after removal from saved list.

--- myplan-web/src/main/webapp/ks-myplan/scripts/myplan.widgets.js
+++ myplan-web/src/main/webapp/ks-myplan/scripts/myplan.widgets.js
@@ -459,6 +459,43 @@ function fnCloseAllPopups() {
         }
     });
 }
+/*
+######################################################################################
+    Function:   Build Term Plan View heading
+######################################################################################
+ */
+function fnBuildTitle(aView, termSelector, headerSelector) {
+    var sText = 'Academic Year';
+    var sFirst = jq.trim ( jq(aView[0]).find("." + termSelector).text() );
+    var sLast = jq.trim ( jq(aView[aView.length-1]).find("." + termSelector).text() );
+    jq("#" + headerSelector + " .myplan-plan-header").html(sText + ' ' + sFirst.substr(-4) + '-' + sLast.substr(-4));
+}
+/*
+######################################################################################
+    Function:   expand/collapse backup course set within plan view
+######################################################################################
+ */
+function fnToggleBackup(e) {
+    stopEvent(e);
+    var target = (e.currentTarget) ? e.currentTarget : e.srcElement;
+    if (!jq(target).hasClass("disabled")) {
+        var oBackup = jq(target).parents(".myplan-term-backup").find(".uif-stackedCollectionLayout");
+        var oQuarter = jq(target).parents("li");
+        var iSpeed = 500;
+        var iDefault = 26;
+        if (jq(target).hasClass("expanded")) {
+            var iAdjust = ( oBackup.height() - ( iDefault * 2 ) ) * -1;
+            jq(target).removeClass("expanded");
+            jq(target).find("span").html("Show");
+        } else {
+            var iAdjust = ( oBackup.find("span a").size() * iDefault ) - oBackup.height();
+            jq(target).addClass("expanded");
+            jq(target).find("span").html("Hide");
+        }
+        oBackup.animate({"height": oBackup.height() + iAdjust}, {duration: iSpeed});
+        oQuarter.animate({"height": oQuarter.height() + iAdjust}, {duration: iSpeed});
+    }
+}
 
 
 
