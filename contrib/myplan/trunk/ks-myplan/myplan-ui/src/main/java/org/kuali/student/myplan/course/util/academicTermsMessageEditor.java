a1f6008684ccc724349342026f3886bd5dc2568a
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-05-09 00:30:59 +0000

    updated teh link to add the focus id so that the quarter view would slide to the respective page

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/academicTermsMessageEditor.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/academicTermsMessageEditor.java
@@ -1,6 +1,7 @@
 package org.kuali.student.myplan.course.util;
 
 import org.apache.log4j.Logger;
+import org.kuali.student.myplan.plan.util.AtpHelper;
 
 import java.util.Collection;
 import java.util.Iterator;
@@ -24,7 +25,9 @@ public class academicTermsMessageEditor extends CollectionListPropertyEditor {
 
         while (i.hasNext()) {
             String term = (String) i.next();
-            sb = sb.append("<dd>").append("You took this course in ").append("<a href=lookup?methodToCall=search&viewId=PlannedCourses-LookupView>").append(term).append("</a>");
+            String[] splitStr = term.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+            String atpId= AtpHelper.getAtpFromYearAndTerm(splitStr[0].trim(),splitStr[1].trim());
+            sb = sb.append("<dd>").append("You took this course in ").append("<a href=lookup?methodToCall=search&viewId=PlannedCourses-LookupView&criteriaFields['focusAtpId']=").append(atpId).append(">").append(term).append("</a>");
         }
         return sb.toString();
     }
