df1d87adc37c43074d687574bb2be249cbc505f2
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-05-14 20:00:39 +0000

    Does better job of distinquishing degree audit report Sections, Requirements, and Advisory notes.

--- contrib/myplan/trunk/ks-myplan/myplan-impl/src/main/java/org/kuali/student/myplan/audit/service/model/Report.java
+++ contrib/myplan/trunk/ks-myplan/myplan-impl/src/main/java/org/kuali/student/myplan/audit/service/model/Report.java
@@ -1,12 +1,15 @@
 package org.kuali.student.myplan.audit.service.model;
 
+import org.apache.regexp.RE;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Report {
     private String webTitle;
 
     String degreeProgram = "XX Bachelor of Arts in Communication XX";
-    String studentName = "XX Jana Winsfeld XX";
+//    String studentName = "XX Jana Winsfeld XX";
     String datePrepared = "XX Feb. 21, 2012 10:18 AM XX";
     String entryDateUW = "XX Autumn 2008 XX";
     String entryDateProgram = "XX Autumn 2010 XX";
@@ -14,17 +17,20 @@ public class Report {
     Requirement summaryUW;
     Requirement summaryProgram;
 
+    public ArrayList<Section> sectionList = new ArrayList<Section>();
+    private ArrayList<String> advisoryList = new ArrayList<String>();
+
     public String getDegreeProgram() { return degreeProgram; }
 
     public void setDegreeProgram(String degreeProgram) {
         this.degreeProgram = degreeProgram;
     }
 
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName( String studentName ) { this.studentName = studentName; }
+//    public String getStudentName() {
+//        return studentName;
+//    }
+//
+//    public void setStudentName( String studentName ) { this.studentName = studentName; }
 
     public String getDatePrepared() {
         return datePrepared;
@@ -43,17 +49,51 @@ public class Report {
     public Requirement getSummaryUW() { return summaryUW; }
     public Requirement getSummaryProgram() { return summaryProgram; }
 
-    public ArrayList<Section> sectionList = new ArrayList<Section>();
 
-    public void addSection( Section section ) {
-        sectionList.add( section );
+//    public void addSection( Section section ) {
+//        sectionList.add( section );
+//    }
+
+    public Section newSection() {
+        Section section = new Section();
+        sectionList.add(section);
+        return section;
     }
 
-    public ArrayList<Section> getSectionList() { return sectionList; }
+    public ArrayList<Section> getSectionList() {
+        return sectionList;
+    }
+
+
+    public Requirement newRequirement()
+    {
+        Requirement requirement = new Requirement();
+        Section section = null;
+        if( sectionList.isEmpty() )
+        {
+            section = newSection();
+            section.setCaption( "(Oops)" );
+        }
+
+        section = sectionList.get( sectionList.size() - 1 );
+        section.addRequirement( requirement );
+        return requirement;
+    }
+
+
 
     public String getWebTitle() { return webTitle; }
     public void setWebTitle(String webTitle) {
         this.webTitle = webTitle;
+    }
+
+
+    public void addAdvisory( String advisory ) {
+        advisoryList.add( advisory );
+    }
+
+    public List<String> getAdvisoryList() {
+        return advisoryList;
     }
 
 }
