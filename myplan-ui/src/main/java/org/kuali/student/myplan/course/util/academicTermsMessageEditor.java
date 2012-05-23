bda577097d68f25741df7d97d9840e63cb978ef6
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-05-23 22:32:35 +0000

    Resolved JIRA-290 and JIRA-195

--- myplan-ui/src/main/java/org/kuali/student/myplan/course/util/academicTermsMessageEditor.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/course/util/academicTermsMessageEditor.java
@@ -1,11 +1,20 @@
 package org.kuali.student.myplan.course.util;
 
 import org.apache.log4j.Logger;
+import org.kuali.rice.core.api.criteria.QueryByCriteria;
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.student.enrollment.acal.constants.AcademicCalendarServiceConstants;
+import org.kuali.student.enrollment.acal.dto.TermInfo;
+import org.kuali.student.enrollment.acal.service.AcademicCalendarService;
 import org.kuali.student.myplan.plan.util.AtpHelper;
 
+import javax.xml.namespace.QName;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Matcher;
+
+import static org.kuali.rice.core.api.criteria.PredicateFactory.equalIgnoreCase;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,6 +27,7 @@ public class academicTermsMessageEditor extends CollectionListPropertyEditor {
 
     private final static Logger logger = Logger.getLogger(academicTermsMessageEditor.class);
 
+
     @Override
     protected String makeHtmlList(Collection c) {
         Iterator<Object> i = c.iterator();
@@ -27,10 +37,19 @@ public class academicTermsMessageEditor extends CollectionListPropertyEditor {
             String term = (String) i.next();
             String[] splitStr = term.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
             String atpId = AtpHelper.getAtpIdFromTermAndYear(splitStr[0].trim(), splitStr[1].trim());
-            sb = sb.append("<dd>").append("You took this course in ")
-                    .append("<a href=lookup?methodToCall=search&viewId=PlannedCourses-LookupView&criteriaFields['focusAtpId']=")
-                    .append(atpId).append(">")
-                    .append(term).append("</a>");
+            List<TermInfo> scheduledTerms = null;
+            String currentTerm = AtpHelper.getCurrentAtpId();
+            if (atpId.compareToIgnoreCase(currentTerm) >= 0) {
+                sb = sb.append("<dd>").append("You're currently enrolled in this course for ")
+                        .append("<a href=lookup?methodToCall=search&viewId=PlannedCourses-LookupView&criteriaFields['focusAtpId']=")
+                        .append(atpId).append(">")
+                        .append(term).append("</a>");
+            } else {
+                sb = sb.append("<dd>").append("You took this course in ")
+                        .append("<a href=lookup?methodToCall=search&viewId=PlannedCourses-LookupView&criteriaFields['focusAtpId']=")
+                        .append(atpId).append(">")
+                        .append(term).append("</a>");
+            }
         }
         return sb.toString();
     }
