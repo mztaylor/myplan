58e75dd84710fb1416760c7b94e9d476a4eea6d3
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-05-23 19:20:56 +0000

    better intepretation of suppression flags

--- myplan-impl/src/main/java/org/kuali/student/myplan/audit/service/model/Count.java
+++ myplan-impl/src/main/java/org/kuali/student/myplan/audit/service/model/Count.java
@@ -1,23 +1,33 @@
 package org.kuali.student.myplan.audit.service.model;
 
 public class Count {
+    public String flag = " ";
     public int required = 100;
     public int earned = 1;
     public int needs = 99;
 
-    public int getRequired() { return required; }
-    public int getEarned() { return earned; }
-    public int getNeeds() { return needs; }
 
     public void setRequired(int required) {
         this.required = required;
     }
+    public int getRequired() { return required; }
 
     public void setEarned(int earned) {
         this.earned = earned;
+    }
+    public int getEarned() { return earned; }
+
+    public boolean showEarned() {
+        return !"E".equals(flag) && earned > 0.0001f;
     }
 
     public void setNeeds(int needs) {
         this.needs = needs;
     }
+    public int getNeeds() { return needs; }
+
+    public boolean showNeeds() {
+        return !"R".equals(flag) && needs > 0.0001f;
+    }
+
 }
--- myplan-impl/src/main/java/org/kuali/student/myplan/audit/service/model/Credits.java
+++ myplan-impl/src/main/java/org/kuali/student/myplan/audit/service/model/Credits.java
@@ -1,12 +1,57 @@
 package org.kuali.student.myplan.audit.service.model;
 
 public class Credits {
-    public float minimum = -1;
-    public float maximum = -99;
+    public String flag = " ";
     public float required = 100;
     public float inprogress = 10;
     public float earned = 1;
     public float needs = 1;
+
+//    public float getMinimum() { return minimum; }
+
+    public void setFlag( String flag ) {
+        this.flag = flag;
+    }
+
+    public void setRequired(float required) {
+        this.required = required;
+    }
+
+    public String getRequired() {
+        return smartZero( required );
+    }
+
+    public void setInprogress(float inprogress) {
+        this.inprogress = inprogress;
+    }
+
+    public String getInprogress() { return smartZero(inprogress); }
+
+    public boolean showInprogress() {
+        return inprogress > 0.0001f;
+    }
+
+    public String getEarned() {
+        return smartZero( earned );
+    }
+
+    public boolean showEarned() {
+        return !"E".equals(flag) && earned > 0.0001f;
+    }
+
+    public String getNeeds() { return smartZero(needs); }
+
+    public boolean showNeeds() {
+        return !"R".equals( flag ) && needs > 0.0001f;
+    }
+
+    public void setEarned(float earned) {
+        this.earned = earned;
+    }
+
+    public void setNeeds(float needs) {
+        this.needs = needs;
+    }
 
     public static String smartZero(float value) {
         String result = Float.toString(value);
@@ -19,38 +64,5 @@ public class Credits {
         return result.substring(0, x + 1);
     }
 
-    public float getMinimum() { return minimum; }
-    public String getRequired() {
-        return smartZero( required );
-    }
 
-    public String getInprogress() { return smartZero(inprogress); }
-
-    public boolean hasInprogress() {
-        return inprogress > 0.0001f;
-    }
-
-    public String getEarned() {
-        return smartZero( earned );
-    }
-
-    public String getNeeds() { return smartZero(needs); }
-
-    public boolean hasNeeds() { return needs > 0.0001f; }
-
-    public void setRequired(float required) {
-        this.required = required;
-    }
-
-    public void setEarned(float earned) {
-        this.earned = earned;
-    }
-
-    public void setInprogress(float inprogress) {
-        this.inprogress = inprogress;
-    }
-
-    public void setNeeds(float needs) {
-        this.needs = needs;
-    }
 }
