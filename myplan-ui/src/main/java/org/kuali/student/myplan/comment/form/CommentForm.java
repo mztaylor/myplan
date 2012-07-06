2908e32319a5b4fe0c28945ea8c1c10f16381708
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-07-06 22:25:38 +0000

    Added a new form view for adding a message

--- myplan-ui/src/main/java/org/kuali/student/myplan/comment/form/CommentForm.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/comment/form/CommentForm.java
@@ -5,4 +5,20 @@ import org.kuali.rice.krad.web.form.UifFormBase;
 public class CommentForm extends UifFormBase {
     private String subject;
     private String body;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
