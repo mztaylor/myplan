9fa627545f00d4bc16681409fd6cf8a9c2e00aaf
sudduth <sudduth@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-07-06 16:40:59 +0000

    CommentConstants.java: Added SERVICE_NAME constant.

--- myplan-ui/src/main/java/org/kuali/student/myplan/comment/CommentConstants.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/comment/CommentConstants.java
@@ -3,6 +3,8 @@ package org.kuali.student.myplan.comment;
 import org.kuali.student.r2.common.util.constants.CommentServiceConstants;
 
 public class CommentConstants extends CommentServiceConstants {
+    public static final String SERVICE_NAME = "CommentService";
+
     public static final String SUCCESS_KEY_MESSAGE_ADDED = "myplan.text.success.comment.messageAdded";
     public static final String SUCCESS_KEY_COMMENT_ADDED = "myplan.text.success.comment.commentAdded";
 }
