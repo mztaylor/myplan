5d30dca579f0278df3c47dff41de5f7acc54ff40
sudduth <sudduth@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-07-09 18:15:02 +0000

    Added method for querying a single message.

--- myplan-ui/src/main/java/org/kuali/student/myplan/comment/service/CommentQueryHelper.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/comment/service/CommentQueryHelper.java
@@ -6,12 +6,15 @@ import org.kuali.student.core.comment.service.CommentService;
 import org.kuali.student.myplan.comment.CommentConstants;
 import org.kuali.student.myplan.comment.dataobject.CommentDataObject;
 import org.kuali.student.myplan.comment.dataobject.MessageDataObject;
-
+import org.apache.log4j.Logger;
 import javax.xml.namespace.QName;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommentQueryHelper {
+
+    private final Logger logger = Logger.getLogger(CommentQueryHelper.class);
+
     private transient CommentService commentService;
 
     private CommentService getCommentService() {
@@ -26,6 +29,31 @@ public class CommentQueryHelper {
         this.commentService = commentService;
     }
 
+    public synchronized MessageDataObject getMessage(String messageId) {
+        CommentInfo commentInfo = null;
+        try {
+            commentInfo = getCommentService().getComment(messageId);
+        } catch (Exception e) {
+            logger.error(String.format("Query for comment [%s] failed.", messageId), e);
+            return null;
+        }
+        return makeMessageDataObject(commentInfo);
+    }
+
+    private MessageDataObject makeMessageDataObject(CommentInfo commentInfo) {
+        MessageDataObject messageDataObject = new MessageDataObject();
+        messageDataObject.setCreateDate(commentInfo.getMetaInfo().getCreateTime());
+        messageDataObject.setSubject(commentInfo.getAttributes().get(CommentConstants.SUBJECT_ATTRIBUTE_NAME));
+        //  FIXME: THis needs to be a name and not the id.
+        messageDataObject.setFrom(commentInfo.getMetaInfo().getCreateId());
+
+        //  Pass the id of the message to get the comments associated with this message.
+        List<CommentDataObject> comments = getComments(commentInfo.getId());
+        messageDataObject.setComments(comments);
+        return messageDataObject;
+    }
+
+
     public synchronized List<MessageDataObject> getMessages(String studentId) {
         List<MessageDataObject> messages = new ArrayList<MessageDataObject>();
 
@@ -37,15 +65,8 @@ public class CommentQueryHelper {
         }
 
         for (CommentInfo ci : commentInfos) {
-            MessageDataObject messageDataObject = new MessageDataObject();
-            messageDataObject.setCreateDate(ci.getMetaInfo().getCreateTime());
-            messageDataObject.setSubject(ci.getAttributes().get(CommentConstants.SUBJECT_ATTRIBUTE_NAME));
-            //  FIXME: THis needs to be a name and not the id.
-            messageDataObject.setFrom(ci.getMetaInfo().getCreateId());
-
-            //  Pass the id of the message to get the comments associated with this message.
-            List<CommentDataObject> comments = getComments(ci.getId());
-            messageDataObject.setComments(comments);
+            MessageDataObject messageDataObject = makeMessageDataObject(ci);
+            messages.add(messageDataObject);
         }
         return messages;
     }
