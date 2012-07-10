5610c0f1a2d6fc2d216013c6fb65e130c5f2372e
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-07-10 22:08:27 +0000

    Changes Made for the comment and message pages

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/comment/controller/CommentController.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/comment/controller/CommentController.java
@@ -26,7 +26,10 @@ import org.kuali.student.common.dto.RichTextInfo;
 import org.kuali.student.core.comment.dto.CommentInfo;
 import org.kuali.student.core.comment.service.CommentService;
 import org.kuali.student.myplan.comment.CommentConstants;
+import org.kuali.student.myplan.comment.dataobject.CommentDataObject;
+import org.kuali.student.myplan.comment.dataobject.MessageDataObject;
 import org.kuali.student.myplan.comment.form.CommentForm;
+import org.kuali.student.myplan.comment.service.CommentQueryHelper;
 import org.kuali.student.myplan.utils.UserSessionHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -37,8 +40,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.namespace.QName;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 @RequestMapping(value = "/comment")
@@ -47,6 +49,8 @@ public class CommentController extends UifControllerBase {
     private final Logger logger = Logger.getLogger(CommentController.class);
 
     private transient CommentService commentService;
+
+    private transient CommentQueryHelper commentQueryHelper;
 
     @Override
     protected CommentForm createInitialForm(HttpServletRequest request) {
@@ -59,6 +63,30 @@ public class CommentController extends UifControllerBase {
         super.start(form, result, request, response);
         CommentForm commentForm = (CommentForm) form;
         commentForm.setStudentName(UserSessionHelper.getStudentName());
+        if (commentForm.getMessageId() != null) {
+            MessageDataObject messageDataObject = null;
+            try {
+                messageDataObject = getCommentQueryHelper().getMessage(commentForm.getMessageId());
+            } catch (Exception e) {
+                logger.error(String.format("Query for comment [%s] failed.", commentForm.getMessageId()), e);
+                return null;
+            }
+            if (messageDataObject != null) {
+                commentForm.setSubject(messageDataObject.getBody());
+                commentForm.setFrom(messageDataObject.getFrom());
+                commentForm.setCreatedDate(messageDataObject.getCreateDate());
+
+                /*Testing purspoe*/
+                CommentDataObject commentDataObject=new CommentDataObject();
+                commentDataObject.setBody("hie hello");
+                commentDataObject.setCreateDate(new Date());
+                commentDataObject.setFrom("ME");
+                messageDataObject.getComments().add(commentDataObject);
+                /*test ends*/
+                commentForm.setComments(messageDataObject.getComments());
+
+            }
+        }
         return getUIFModelAndView(commentForm);
     }
 
@@ -66,17 +94,51 @@ public class CommentController extends UifControllerBase {
     public ModelAndView addComment(@ModelAttribute("KualiForm") CommentForm form, BindingResult result,
                                    HttpServletRequest httprequest, HttpServletResponse httpresponse) {
 
+        Person user = GlobalVariables.getUserSession().getPerson();
+        String principleId = user.getPrincipalId();
+        CommentInfo commentInfo = null;
+        try {
+            commentInfo = getCommentService().getComment(form.getMessageId());
+        } catch (Exception e) {
+            logger.error(String.format("Query for comment [%s] failed.", form.getMessageId()), e);
+            return null;
+        }
+        /*If not a Adviser of if the user accesing is not the owner of the message*/
+        /*if (!UserSessionHelper.isAdviser() && !principleId.equalsIgnoreCase(commentInfo.getReferenceId())) {
+            String[] params = {};
+            return doErrorPage(form, CommentConstants.ADVISER_ACCESS_ERROR, params);
+        }*/
+        CommentInfo ci = new CommentInfo();
+        MetaInfo m = new MetaInfo();
+        m.setCreateId(principleId);
+        m.setUpdateId(principleId);
+        ci.setMetaInfo(m);
+        ci.setType(CommentConstants.COMMENT_TYPE);
+        ci.setState("ACTIVE");
+        RichTextInfo body = new RichTextInfo();
+        body.setPlain(form.getBody());
+        body.setFormatted(form.getBody());
+        ci.setCommentText(body);
+        ci.setReferenceId(commentInfo.getId());
+        ci.setReferenceTypeKey(CommentConstants.COMMENT_REF_TYPE);
 
-        return null;
+        try {
+            CommentInfo x = getCommentService().addComment(commentInfo.getId(), CommentConstants.COMMENT_REF_TYPE, ci);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return doSuccess(form, CommentConstants.SUCCESS_KEY_MESSAGE_ADDED, new String[0]);
     }
 
     @RequestMapping(params = "methodToCall=addMessage")
     public ModelAndView addMessage(@ModelAttribute("KualiForm") CommentForm form, BindingResult result,
                                    HttpServletRequest httprequest, HttpServletResponse httpresponse) {
-        if (!UserSessionHelper.isAdviser()) {
+        /*if (!UserSessionHelper.isAdviser() || !form.getStudentId().equalsIgnoreCase(UserSessionHelper.getStudentId())) {
             String[] params = {};
             return doErrorPage(form, CommentConstants.ADVISER_ACCESS_ERROR, params);
-        }
+        }*/
 
 
         Map<String, String> attributes = new HashMap<String, String>();
@@ -97,6 +159,8 @@ public class CommentController extends UifControllerBase {
         body.setPlain(form.getBody());
         body.setFormatted(form.getBody());
         ci.setCommentText(body);
+        ci.setReferenceId(principleId);
+        ci.setReferenceTypeKey(CommentConstants.MESSAGE_REF_TYPE);
 
         try {
             CommentInfo x = getCommentService().addComment(UserSessionHelper.getStudentId(), CommentConstants.MESSAGE_REF_TYPE, ci);
@@ -106,6 +170,7 @@ public class CommentController extends UifControllerBase {
 
         return doSuccess(form, CommentConstants.SUCCESS_KEY_MESSAGE_ADDED, new String[0]);
     }
+
 
     /**
      * Initializes the error page.
@@ -120,6 +185,17 @@ public class CommentController extends UifControllerBase {
     private ModelAndView doSuccess(CommentForm form, String messageKey, String[] params) {
         GlobalVariables.getMessageMap().putInfoForSectionId("Page Id", messageKey, params);
         return getUIFModelAndView(form, "Page Id");
+    }
+
+    public CommentQueryHelper getCommentQueryHelper() {
+        if (commentQueryHelper == null) {
+            commentQueryHelper = new CommentQueryHelper();
+        }
+        return commentQueryHelper;
+    }
+
+    public void setCommentQueryHelper(CommentQueryHelper commentQueryHelper) {
+        this.commentQueryHelper = commentQueryHelper;
     }
 
     public CommentService getCommentService() {
