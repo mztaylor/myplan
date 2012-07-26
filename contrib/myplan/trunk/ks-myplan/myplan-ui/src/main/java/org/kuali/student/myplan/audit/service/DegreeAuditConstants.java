3053695d57cd0845533ee5994981d88890bd8ee3
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-07-26 07:29:56 +0000

    made changes to auto populate the dropdown with the audit report program

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/audit/service/DegreeAuditConstants.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/audit/service/DegreeAuditConstants.java
@@ -9,4 +9,12 @@ public class DegreeAuditConstants extends DegreeAuditServiceConstants {
     public static final String AUDIT_PARAM_ID = "auditId";
     public static final String AUDIT_PARAM_TYPE = "auditType";
     public static final String AUDIT_EMPTY_PAGE ="degree_audit_empty_page";
+
+    public static final String ORG_QUERY_PARAM = "org_queryParam_orgType";
+
+    public static final String CAMPUS_LOCATION = "kuali.uw.org.type.campus";
+
+
+
+
 }
