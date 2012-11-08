efc16e4972eef2bf559589e55eb57882943ba59d
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-11-08 01:34:15 +0000

    removed the timeout

--- contrib/myplan/trunk/ks-myplan/myplan-web/src/main/webapp/ks-myplan/scripts/myplan.widgets.js
+++ contrib/myplan/trunk/ks-myplan/myplan-web/src/main/webapp/ks-myplan/scripts/myplan.widgets.js
@@ -1336,8 +1336,7 @@ function myplanAjaxSubmitQuickAdd(id, submitOptions, methodToCall, e, bDialog) {
                         eval('jQuery.publish("' + key + '", [' + JSON.stringify(jQuery.extend(json[key], oMessage)) + ']);');
                     }
                 }
-                setUrlHash('modified', 'yes');
-                setTimeout('fnCloseAllPopups()',500);
+                setUrlHash('modified', 'yes');                
                 break;
             case 'error':
                 var oMessage = { 'message':'<img src="../ks-myplan/images/pixel.gif" alt="" class="icon"><span class="message">' + jQuery('body').data('validationMessages').serverErrors[0] + '</span>', 'cssClass':'myplan-feedback error' };
