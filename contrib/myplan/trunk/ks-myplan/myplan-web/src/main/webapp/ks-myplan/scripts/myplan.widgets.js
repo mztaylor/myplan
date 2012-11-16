466e542b5d57edb3966760e11ef1577e11d52d6f
garettg <garettg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2012-11-16 22:17:52 +0000

    MYPLAN-1422 Fixed recent audit list titles wrapping

--- contrib/myplan/trunk/ks-myplan/myplan-web/src/main/webapp/ks-myplan/scripts/myplan.widgets.js
+++ contrib/myplan/trunk/ks-myplan/myplan-web/src/main/webapp/ks-myplan/scripts/myplan.widgets.js
@@ -755,8 +755,8 @@ function truncateAuditTitle(id) {
         }
 
         var width = jQuery(this).width();
-        var label = jQuery(this).find(".uif-label").width();
-        jQuery(this).find(".uif-label label").css({"color":"#777777"});
+        var label = parseFloat(jQuery(this).find(".uif-label label").css({"color":"#777777"}).width()) + parseFloat(jQuery(this).find(".uif-label label").css("padding-right"));
+
         jQuery(this).find(".uif-label").next("span").width(width - label - 1).css({
             "text-overflow":"ellipsis",
             "white-space":"nowrap",
