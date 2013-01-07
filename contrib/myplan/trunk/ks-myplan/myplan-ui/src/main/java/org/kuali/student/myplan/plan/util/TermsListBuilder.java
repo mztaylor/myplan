b609983314cd00570c830c8e0e5bc35f708ea071
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-01-07 20:27:14 +0000

    Fix for MYPLAN-1552, creating the list of future terms failed once the UW's current ATP changed, which now shouldn't cause a problem.

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/plan/util/TermsListBuilder.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/plan/util/TermsListBuilder.java
@@ -7,59 +7,55 @@ import org.kuali.rice.krad.keyvalues.KeyValuesBase;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Assembles a list of published terms.
  */
 public class TermsListBuilder extends KeyValuesBase {
 
+    // 6 years * 4 quarters = 24 terms
+    private static int MAX_FUTURE_TERMS = 24;
+
+    public static final Pattern atpRegex = Pattern.compile("kuali\\.uw\\.atp\\.([0-9]{4})\\.([1-4])");
+    public static final String atpFormat = "kuali.uw.atp.%d.%d";
+
     private final Logger logger = Logger.getLogger(TermsListBuilder.class);
 
-    private static int futureTermsCount = 6;
-
-    private static String atpTerm1 = "1";
-    private static String atpTerm2 = "2";
-    private static String atpTerm3 = "3";
-    private static String atpTerm4 = "4";
 
     /**
-     * Build and returns the list of available terms.
+     * Creates KeyValue list containing MAX_FUTURE_TERMS terms, beginning with the current ATP
      *
      * @return A List of available terms as KeyValue items.
      */
+
     @Override
+
     public List<KeyValue> getKeyValues() {
 
-        List<KeyValue> keyValues = new ArrayList<KeyValue>();
-        String minTerm = AtpHelper.getCurrentAtpId();
-        String[] minTerms = AtpHelper.atpIdToTermAndYear(minTerm);
-        int maximumYear = Integer.parseInt(minTerms[1]) + futureTermsCount;
-        int maximumQuarter = Integer.parseInt(minTerms[0]);
-        if (maximumQuarter == 4) {
-            maximumQuarter = 3;
-        } else {
-            maximumQuarter = maximumQuarter++;
-        }
-        String maxTerm = AtpHelper.getAtpFromNumTermAndYear(String.valueOf(maximumQuarter), String.valueOf(maximumYear));
-        String[] maxTerms = AtpHelper.atpIdToTermAndYear(maxTerm);
-        int minYear = Integer.parseInt(minTerms[1]);
-        String term1 = "";
-        String term2 = "";
-        String term3 = "";
-        String term4 = "";
-        for (int i = 0; !term4.equalsIgnoreCase(maxTerm); i++) {
-            term1 = AtpHelper.getAtpFromNumTermAndYear(atpTerm4, String.valueOf(minYear));
-            keyValues.add(new ConcreteKeyValue(term1, AtpHelper.atpIdToTermName(term1)));
-            minYear++;
-            term2 = AtpHelper.getAtpFromNumTermAndYear(atpTerm1, String.valueOf(minYear));
-            keyValues.add(new ConcreteKeyValue(term2, AtpHelper.atpIdToTermName(term2)));
-            term3 = AtpHelper.getAtpFromNumTermAndYear(atpTerm2, String.valueOf(minYear));
-            keyValues.add(new ConcreteKeyValue(term3, AtpHelper.atpIdToTermName(term3)));
-            term4 = AtpHelper.getAtpFromNumTermAndYear(atpTerm3, String.valueOf(minYear));
-            keyValues.add(new ConcreteKeyValue(term4, AtpHelper.atpIdToTermName(term4)));
-        }
-        return keyValues;
-    }
+        List<KeyValue> list = new ArrayList<KeyValue>(MAX_FUTURE_TERMS);
 
+        String currentATP = AtpHelper.getCurrentAtpId();
+        Matcher m = atpRegex.matcher(currentATP);
+        if (m.find()) {
+            int year = Integer.parseInt(m.group(1));
+            int term = Integer.parseInt(m.group(2));
+
+            for (int x = 0; x < MAX_FUTURE_TERMS; x++) {
+                String atp = String.format(atpFormat, year, term);
+                String value = AtpHelper.atpIdToTermName( atp );
+                ConcreteKeyValue concrete = new ConcreteKeyValue( atp, value );
+                list.add( concrete );
+                term++;
+                if (term > 4) {
+                    term = 1;
+                    year++;
+                }
+            }
+        }
+
+        return list;
+    }
 }
 
