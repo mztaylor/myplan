d029628bb299971cc4a29e045c5c9f490dfc1b1b
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-01-29 18:25:47 +0000

    Moved displayed logic for "to be arranged" for day, time, building, location to the MeetingDetailsPropertyEditor. Also, now only displays "to be arranged" one time (at most).

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/MeetingDetailsPropertyEditor.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/MeetingDetailsPropertyEditor.java
@@ -10,6 +10,9 @@ import org.kuali.student.myplan.course.dataobject.MeetingDetails;
 public class MeetingDetailsPropertyEditor extends PropertyEditorSupport {
 	private final static Logger logger = Logger.getLogger(MeetingDetailsPropertyEditor.class);
 	
+	public final static String TO_BE_ARRANGED = "to be arranged";
+
+	
 	@Override
 	public void setValue(Object value) {
 		if (value == null) {
@@ -42,15 +45,40 @@ public class MeetingDetailsPropertyEditor extends PropertyEditorSupport {
     	sb.append( "<div class='meetingdetailslist'>" );
     	
     	for( MeetingDetails m : list ) {
-    	
+
+    		boolean tba = false;
+    		
 	    	String building = m.getBuilding();
-	    	if( !"NOC".equals( building) && !building.startsWith("*") && "seattle".equalsIgnoreCase(m.getCampus()))
-	    	{
+	    	if( building == null || "".equals( building )) {
+	    		building = TO_BE_ARRANGED;
+	    		tba = true;
+	    	}
+	    	else if( !"NOC".equals( building) && !building.startsWith("*") && "seattle".equalsIgnoreCase(m.getCampus())) {
 	    		building = String.format( "<a href='http://uw.edu/maps/?%s' target='_blank' style='margin-right:0.33em;'>%s</a>", building, building );
 	    	}
-	    	// TODO: campus
+
+	    	String days = m.getDays();
+	    	if( days == null ) {
+	    		days = "";
+	    	}
+	    	if( "".equals( days ) && !tba ) {
+	    		days = TO_BE_ARRANGED;
+	    	}
 	    	
-	    	String temp = String.format( template, m.getDays(), m.getTime(), building, m.getRoom() );
+	    	String time = m.getTime();
+	    	if( time == null ) {
+	    		time = "";
+	    	}
+	    	
+			String room = m.getRoom();
+	    	if( room == null ) {
+	    		room = "";
+	    	}
+	    	if( "".equals( room ) && !tba ) {
+	    		room = TO_BE_ARRANGED;
+	    	}
+			
+			String temp = String.format( template, days, time, building, room );
 	    	sb.append(temp );
     	}
     	sb.append( "</div>" );
