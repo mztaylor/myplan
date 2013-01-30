4148fc73485a706b839b838823c0ed2a612b8ec8
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-01-30 19:10:23 +0000

    Update Course Offering Section Details to handle multiple terms. Added AtpHelper.YearTerm helper object.

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/plan/util/TermsListBuilder.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/plan/util/TermsListBuilder.java
@@ -1,16 +1,14 @@
 package org.kuali.student.myplan.plan.util;
 
 import org.apache.log4j.Logger;
+
 import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
 import org.kuali.rice.krad.keyvalues.KeyValuesBase;
-import org.kuali.student.core.atp.dto.AtpInfo;
-import org.kuali.student.core.atp.service.impl.AtpServiceImpl;
-
+import static org.kuali.student.myplan.plan.util.AtpHelper.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Assembles a list of published terms.
@@ -19,9 +17,6 @@ public class TermsListBuilder extends KeyValuesBase {
 
     // 5 years * 4 quarters = 20 terms
     private static int MAX_FUTURE_TERMS = 20;
-
-    public static final Pattern atpRegex = Pattern.compile("kuali\\.uw\\.atp\\.([0-9]{4})\\.([1-4])");
-    public static final String atpFormat = "kuali.uw.atp.%d.%d";
 
     private final Logger logger = Logger.getLogger(TermsListBuilder.class);
 
@@ -41,13 +36,13 @@ public class TermsListBuilder extends KeyValuesBase {
         /*TODO Once First and last dates in the Atp table are added change the way of populating the future terms by Using the method AtpService.getAtpsByDates*/
         String firstPlanATP = AtpHelper.getFirstPlanTerm();
         if (firstPlanATP != null) {
-            Matcher m = atpRegex.matcher(firstPlanATP);
+            Matcher m = ATP_REGEX.matcher(firstPlanATP);
             if (m.find()) {
                 int year = Integer.parseInt(m.group(1));
                 int term = Integer.parseInt(m.group(2));
 
                 for (int x = 0; x < MAX_FUTURE_TERMS; x++) {
-                    String atp = String.format(atpFormat, year, term);
+                    String atp = String.format(ATP_FORMAT, year, term);
                     if (AtpHelper.doesAtpExist(atp)) {
                         String value = AtpHelper.atpIdToTermName(atp);
                         ConcreteKeyValue concrete = new ConcreteKeyValue(atp, value);
