66046031666c5c0adc4011e5021b076544cd975c
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-03-04 23:33:52 +0000

    Fixed the compile issues and adding some properties in planForm for single quarter use

--- myplan-ui/src/main/java/org/kuali/student/myplan/plan/PlanConstants.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/plan/PlanConstants.java
@@ -88,6 +88,9 @@ public class PlanConstants extends AcademicPlanServiceConstants {
     public static final String SESSION_KEY_IS_ADVISER = "kuali.uw.authz.adviser";
     public static final String SESSION_KEY_STUDENT_ID = "kuali.uw.authn.studentId";
     public static final String SESSION_KEY_STUDENT_NAME = "kuali.uw.authn.studentName";
+    
+    /*Regex to Split Digits and alphabets Eg: COM 348 --> COM  348*/
+    public static final String SPLIT_DIGITS_ALPHABETS = "(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)";
 
     /**
      * Names of javascript events that can be scheduled in response to the outcome of a plan item request.
