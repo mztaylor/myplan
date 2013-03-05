8b76e130733dab99ff12644dceb6ce325d3385dc
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-03-05 18:58:08 +0000

    Changed the property value for Scheduled for from courseOfferingInstitutionlist to scheduledTerms

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/ScheduledTermsPropertyEditor.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/ScheduledTermsPropertyEditor.java
@@ -18,104 +18,32 @@ package org.kuali.student.myplan.course.util;
 import org.apache.log4j.Logger;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
-import org.kuali.rice.core.api.config.property.ConfigContext;
-import org.kuali.student.myplan.course.dataobject.CourseOfferingInstitution;
-import org.kuali.student.myplan.course.dataobject.CourseOfferingTerm;
-import org.kuali.student.myplan.plan.util.AtpHelper;
-import org.springframework.util.StringUtils;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.beans.PropertyEditorSupport;
 import java.io.Serializable;
-import java.util.*;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.regex.Matcher;
 
 public class ScheduledTermsPropertyEditor extends CollectionListPropertyEditor {
 
     private final static Logger logger = Logger.getLogger(ScheduledTermsPropertyEditor.class);
 
-    private static String DLClass = "<dl class=\"scheduled\"> %s </dl>";
-
-    private static String DDClass = "<dd class=\"%s\">%s</dd>";
-
-    protected boolean fullLinks = false;
-
-    public boolean isFullLinks() {
-        return fullLinks;
-    }
-
-    public void setFullLinks(boolean fullLinks) {
-        this.fullLinks = fullLinks;
-    }
-
     @Override
-    public String getAsText() {
-        /*Logic to build the scheduled terms for given course offering institutions course offering terms. 
-        *
-        * if fullLinks boolean is false then the scheduled term badges are returned
-        * returns following string if the course is scheduled for Summer 2013.
-        * <dl class="scheduled"><dd class="SU"> SU 13 </dd></dl>
-        *
-        * if fullLinks boolean is true then the scheduled term links are returned
-        * returns following string if the course is scheduled for Winter 2013 , Spring 2013.
-        * view all sections for Winter 2013, Spring 2013.
-        * */
+    protected String makeHtmlList(Collection c) {
+        StringBuilder list = new StringBuilder();
+        Iterator<Object> i = c.iterator();
+        while (i.hasNext()) {
+            String term = (String) i.next();
+            String elemTxt = term;
 
-        String courseId = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getParameter("courseId");
-        StringBuilder sb = new StringBuilder();
-        List<CourseOfferingInstitution> courseOfferingInstitutions = (List<CourseOfferingInstitution>) super.getValue();
-        Map<String, String> scheduledTerms = new HashMap<String, String>();
-        int counter = 0;
-        for (CourseOfferingInstitution courseOfferingInstitution : courseOfferingInstitutions) {
-            for (CourseOfferingTerm courseOfferingTerm : courseOfferingInstitution.getCourseOfferingTermList()) {
-                if (scheduledTerms.get(AtpHelper.getAtpIdFromTermYear(courseOfferingTerm.getTerm())) == null) {
-                    String instituteCode = courseOfferingInstitution.getCode();
-                    if(!StringUtils.hasText(instituteCode)){
-                        instituteCode = null;
-                    }
-                    scheduledTerms.put(AtpHelper.getAtpIdFromTermYear(courseOfferingTerm.getTerm()), String.format("%s:%s", courseOfferingTerm.getTerm(), instituteCode));
-                }
+            // Convert Winter 2012 to WI 12
+            Matcher m = CourseSearchConstants.TERM_PATTERN.matcher(term);
+            if(m.matches()) {
+                elemTxt = m.group(1).substring(0,2).toUpperCase() + " " + m.group(2);
             }
-
+            list.append(wrapListItem(elemTxt));
         }
-        if (scheduledTerms.size() > 0) {
-            SortedSet<String> sortedKeys = new TreeSet<String>(scheduledTerms.keySet());
-            for (String scheduledTerm : sortedKeys) {
-                String[] values = scheduledTerms.get(scheduledTerm).split(":");
-                String term = values[0];
-                String instCode = values[1];
-                String serverUrl = ConfigContext.getCurrentContextConfig().getProperty(CourseSearchConstants.APP_URL);
-                String atpId = AtpHelper.getAtpIdFromTermYear(term).replace(".", "-");
-                if (!fullLinks) {
-                    Matcher m = CourseSearchConstants.TERM_PATTERN.matcher(term);
-                    if (m.matches()) {
-                        String elemTxt = m.group(1).substring(0, 2).toUpperCase() + " " + m.group(2);
-                        String link = String.format(CourseSearchConstants.LINK, serverUrl, courseId, instCode, atpId, elemTxt);
-                        /*TODO: Once way to  display and link to PCE, ROTC, and other special sections is specified cahnge the second elemTxt in the below to link*/
-                        sb.append(String.format(DDClass, elemTxt, elemTxt));
-                    }
-                } else {
-                    String link = String.format(CourseSearchConstants.LINK, serverUrl, courseId, instCode, atpId, term);
-                    if (counter == 0) {
-                        sb.append(String.format("View all sections for %s", link));
-                        counter++;
-
-                    } else if (counter > 0 && !(counter == sortedKeys.size() - 1)) {
-                        sb.append(String.format(", %s", link));
-                        counter++;
-
-                    } else if (counter > 0 && (counter == sortedKeys.size() - 1)) {
-                        sb.append(String.format(" and %s", link));
-                        counter++;
-                    }
-
-                }
-            }
-
-        }
-        return String.format(DLClass, sb.toString());
+        return list.toString();
     }
-
 }
-
