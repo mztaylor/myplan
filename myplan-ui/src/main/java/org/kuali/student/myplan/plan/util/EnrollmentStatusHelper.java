6acfec66a590f838c5404e3336f497f120906390
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-03-06 19:22:59 +0000

    improved runAudit method's

--- myplan-ui/src/main/java/org/kuali/student/myplan/plan/util/EnrollmentStatusHelper.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/plan/util/EnrollmentStatusHelper.java
@@ -1,14 +1,13 @@
 package org.kuali.student.myplan.plan.util;
 
-import edu.uw.kuali.student.lib.client.studentservice.StudentServiceClient;
-import org.kuali.student.enrollment.courseoffering.dto.ActivityOfferingDisplayInfo;
+import org.kuali.student.myplan.course.dataobject.ActivityOfferingItem;
 
 public interface EnrollmentStatusHelper {
 
 
-    void setStudentServiceClient(StudentServiceClient studentServiceClient);
+//    void setStudentServiceClient(StudentServiceClient studentServiceClient);
 
 
-    void populateEnrollmentFields(ActivityOfferingDisplayInfo activity, String year, String quarter, String curric, String num, String sectionID) throws Exception;
+    public void populateEnrollmentFields(ActivityOfferingItem activity, String year, String quarter, String curric, String num, String sectionID) throws Exception;
 
 }
