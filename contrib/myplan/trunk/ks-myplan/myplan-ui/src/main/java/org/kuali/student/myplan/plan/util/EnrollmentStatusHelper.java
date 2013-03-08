7e7d7668055c599a176b6a5207a69c93e56f9be6
jo25 <jo25@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-03-08 21:35:09 +0000

    added methods for JSON section status reply

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/plan/util/EnrollmentStatusHelper.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/plan/util/EnrollmentStatusHelper.java
@@ -1,12 +1,16 @@
 package org.kuali.student.myplan.plan.util;
 
+import edu.uw.kuali.student.lib.client.studentservice.ServiceException;
+import org.dom4j.DocumentException;
 import org.kuali.student.myplan.course.dataobject.ActivityOfferingItem;
+
+import java.util.LinkedHashMap;
 
 public interface EnrollmentStatusHelper {
 
 
-//    void setStudentServiceClient(StudentServiceClient studentServiceClient);
-
+    public LinkedHashMap<String, LinkedHashMap<String, Object>> getAllSectionStatus(LinkedHashMap<String, LinkedHashMap<String, Object>> mapmap, String year, String quarter,
+                                                                                    String curric, String num) throws ServiceException, DocumentException;
 
     public void populateEnrollmentFields(ActivityOfferingItem activity, String year, String quarter, String curric, String num, String sectionID) throws Exception;
 
