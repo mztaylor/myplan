71127fdc90baeb7138e1743416336f3167aef154
kmuthusw <kmuthusw@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-03-08 23:31:17 +0000

    CourseHelper refactor

--- myplan-ui/src/main/java/org/kuali/student/myplan/plan/util/EnrollmentStatusHelper.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/plan/util/EnrollmentStatusHelper.java
@@ -3,6 +3,7 @@ package org.kuali.student.myplan.plan.util;
 import edu.uw.kuali.student.lib.client.studentservice.ServiceException;
 import org.dom4j.DocumentException;
 import org.kuali.student.myplan.course.dataobject.ActivityOfferingItem;
+import org.kuali.student.myplan.plan.dataobject.DeconstructedCourseCode;
 
 import java.util.LinkedHashMap;
 
@@ -13,5 +14,9 @@ public interface EnrollmentStatusHelper {
                                                                                     String curric, String num) throws ServiceException, DocumentException;
 
     public void populateEnrollmentFields(ActivityOfferingItem activity, String year, String quarter, String curric, String num, String sectionID) throws Exception;
+
+    public DeconstructedCourseCode getCourseDivisionAndNumber(String courseCode);
+
+    public String getCourseId(String subjectArea, String number);
 
 }
