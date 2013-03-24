2bc968f7883b611fa26f6d064f06ac52a3fef39e
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-03-24 21:00:35 +0000

    Changes made to resolve issues after 2.2.0 upgrade

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/ScheduledTermsPropertyEditor.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/ScheduledTermsPropertyEditor.java
@@ -15,24 +15,94 @@ package org.kuali.student.myplan.course.util;
  * limitations under the License.
  */
 
+import org.apache.commons.lang.StringUtils;
+import org.kuali.student.myplan.course.dataobject.CourseSummaryDetails;
+
+import java.beans.PropertyEditorSupport;
 import java.util.Collection;
+import java.util.List;
 import java.util.regex.Matcher;
 
-public class ScheduledTermsPropertyEditor extends CollectionListPropertyEditor {
+public class ScheduledTermsPropertyEditor extends PropertyEditorSupport {
+    protected CollectionListPropertyEditorHtmlListType listType = CollectionListPropertyEditorHtmlListType.DL;
+    protected String emptyListMessage = "";
+    private List<String> styleClasses;
+    private boolean applyClassOnItem = false;
+
+    public String getEmptyListMessage() {
+        return emptyListMessage;
+    }
+
+    public void setEmptyListMessage(String emptyListMessage) {
+        this.emptyListMessage = emptyListMessage;
+    }
+
+    public List<String> getStyleClasses() {
+        return styleClasses;
+    }
+
+    public void setStyleClasses(List<String> styleClasses) {
+        this.styleClasses = styleClasses;
+    }
+
+    public boolean isApplyClassOnItem() {
+        return applyClassOnItem;
+    }
+
+    public void setApplyClassOnItem(boolean applyClassOnItem) {
+        this.applyClassOnItem = applyClassOnItem;
+    }
+
+    public String getStyleClassesAsString() {
+        if (styleClasses != null) {
+            return StringUtils.join(styleClasses, " ");
+        }
+        return "";
+    }
 
     @Override
+    public void setValue(Object value) {
+        super.setValue(value);
+    }
+
+    /*TODO: KUALI-RICE: 6286 Upgrade to list with rice 2.2.1*/
+    @Override
+    public String getAsText() {
+        CourseSummaryDetails courseSummaryDetails = (CourseSummaryDetails) super.getValue();
+        StringBuffer formattedText = new StringBuffer();
+        formattedText.append("<" + listType.getListElementName() + " class=\"" + getStyleClassesAsString() + "\">");
+        if (courseSummaryDetails != null && courseSummaryDetails.getScheduledTerms() != null && courseSummaryDetails.getScheduledTerms().size() > 0) {
+            formattedText.append(makeHtmlList(courseSummaryDetails.getScheduledTerms()));
+        }else {
+            formattedText.append("Not currently scheduled");
+        }
+        formattedText.append("</" + listType.getListElementName() + ">");
+        return formattedText.toString();
+    }
+
     protected String makeHtmlList(Collection c) {
         StringBuilder sb = new StringBuilder();
-        for( Object item : c )
-        {
-        	String text = item.toString();
+        for (Object item : c) {
+            String text = item.toString();
             // Convert Winter 2012 to WI 12
             Matcher m = CourseSearchConstants.TERM_PATTERN.matcher(text);
-            if(m.matches()) {
-            	text = m.group(1).substring(0,2).toUpperCase() + " " + m.group(2);
+            if (m.matches()) {
+                text = m.group(1).substring(0, 2).toUpperCase() + " " + m.group(2);
             }
             sb.append(wrapListItem(text));
         }
         return sb.toString();
+    }
+
+    protected String wrapListItem(String value) {
+        StringBuilder elementText = new StringBuilder();
+
+        // Strip out any numbers from the value and use the text for class on the list item
+        // Apply style class on item only if specified to do so
+        String liClass = (applyClassOnItem && value != emptyListMessage) ? " class=\"" + value.replaceAll("\\d*$", "").trim() + "\"" : "";
+        elementText.append("<" + listType.getListItemElementName() + liClass + ">");
+        elementText.append(value);
+        elementText.append("</" + listType.getListItemElementName() + ">");
+        return elementText.toString();
     }
 }
