ee3d33b89ec25c1a6d3c7153855630ef9d5015f0
garettg <garettg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-09-03 22:26:43 +0000

    Added functionality to accept a recommendation.

--- contrib/myplan/trunk/ks-myplan/myplan-web/src/main/webapp/ks-myplan/scripts/myplan.events.js
+++ contrib/myplan/trunk/ks-myplan/myplan-web/src/main/webapp/ks-myplan/scripts/myplan.events.js
@@ -28,7 +28,7 @@ function planItemTemplate(data) {
     }
 
     var action = jQuery("<div/>").attr("id", itemId).attr({
-        "title": data.planItemShortTitle + " " + ((data.sections != null && data.sections != "") ? data.sections + " " : "") + "'" + data.planItemLongTitle + "'",
+        "title": ((data.placeHolder == "true" && data.planItemShortTitle == data.planItemLongTitle) ? "" : data.planItemShortTitle) + " " + ((data.sections != null && data.sections != "") ? data.sections + " " : "") + "'" + data.planItemLongTitle + "'" + ((data.placeHolder == "true") ? " placeholder":""),
         "class": "uif-horizontalFieldGroup itemAction uif-tooltip uif-boxLayoutHorizontalItem",
         "data-atpid": data.atpId.replace(/-/g, "."),
         "data-planitemid": data.planItemId,
@@ -71,7 +71,7 @@ function planItemTemplate(data) {
         }).append(image.clone());
         itemGroup.append(note);
         var decoded = jQuery("<div/>").html(data.note).text();
-        var createTooltip = " createTooltip('" + itemId + "_note', ' <p>" + decoded + "</p><p><a data-planitemtype=" + data.planItemType + " data-planitemid=" + data.planItemId + " data-atpid=" + data.atpId.replace(/-/g, ".") + " onclick=editNote(jQuery(this),event);>Edit Note</a></p> ', {position:'top',align:'left',alwaysVisible:false,tail:{align:'left',hidden:false},themePath:'../ks-myplan/jquery-popover/jquerypopover-theme/',themeName:'ksap-notes',selectable:true,width:'250px',openingSpeed:50,closingSpeed:50,openingDelay:500,closingDelay:0,themeMargins:{total:'17px',difference:'10px'},distance:'0px'},true,true);";
+        var createTooltip = " createTooltip('" + itemId + "_note', ' <p>" + decoded + "</p><p><a data-planitemtype=" + data.planItemType + " data-planitemid=" + data.planItemId + " data-atpid=" + data.atpId.replace(/-/g, ".") + " onclick=editNote(jQuery(this),event);>Edit Note</a></p> ', {position:'top',align:'left',alwaysVisible:false,tail:{align:'left',hidden:false},themePath:'../ks-myplan/jquery-popover/jquerypopover-theme/',themeName:'ksap-notes',selectable:true,openingSpeed:50,closingSpeed:50,openingDelay:500,closingDelay:0,themeMargins:{total:'17px',difference:'10px'},distance:'0px'},true,true);";
         script.val(script.val() + createTooltip);
     }
 
