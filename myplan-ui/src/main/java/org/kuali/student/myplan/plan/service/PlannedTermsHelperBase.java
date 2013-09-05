7a2a1abf0228292b69673634128a7da8c14d6303
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-09-05 04:06:08 +0000

    reverted to previous revision changes

--- myplan-ui/src/main/java/org/kuali/student/myplan/plan/service/PlannedTermsHelperBase.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/plan/service/PlannedTermsHelperBase.java
@@ -78,6 +78,8 @@ public class PlannedTermsHelperBase {
             focusQuarterYear[1] = year;
         }
 
+        Set<String> plannedAndBackupItems = new HashSet<String>();
+
         /*
         *  Populating the PlannedTerm List.
         */
@@ -96,6 +98,7 @@ public class PlannedTermsHelperBase {
 
             for (PlannedCourseDataObject plan : plannedCoursesList) {
                 String atp = plan.getPlanItemDataObject().getAtp();
+                plannedAndBackupItems.add(String.format("%s|%s", plan.getPlanItemDataObject().getRefObjId(), atp));
                 boolean exists = false;
                 for (PlannedTerm term : plannedTerms) {
                     if (term.getAtpId().equalsIgnoreCase(atp)) {
@@ -135,6 +138,7 @@ public class PlannedTermsHelperBase {
             int count = plannedTerms.size();
             for (PlannedCourseDataObject bl : backupCoursesList) {
                 String atp = bl.getPlanItemDataObject().getAtp();
+                plannedAndBackupItems.add(String.format("%s|%s", bl.getPlanItemDataObject().getRefObjId(), atp));
                 boolean added = false;
                 for (int i = 0; i < count; i++) {
                     if (atp.equalsIgnoreCase(plannedTerms.get(i).getAtpId())) {
@@ -176,6 +180,10 @@ public class PlannedTermsHelperBase {
             int count = plannedTerms.size();
             for (PlannedCourseDataObject bl : recommendedCoursesList) {
                 String atp = bl.getPlanItemDataObject().getAtp();
+                /*Not adding the recommended course if it is already planned for the qtr*/
+                if (plannedAndBackupItems.contains(String.format("%s|%s", bl.getPlanItemDataObject().getRefObjId(), atp)) && !getUserSessionHelper().isAdviser()) {
+                    continue;
+                }
                 boolean added = false;
                 for (int i = 0; i < count; i++) {
                     if (atp.equalsIgnoreCase(plannedTerms.get(i).getAtpId())) {
