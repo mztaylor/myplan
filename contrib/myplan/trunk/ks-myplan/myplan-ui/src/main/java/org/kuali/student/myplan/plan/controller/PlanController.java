16914d2e4cf4b77031d3ad8858d8dc3dc18d6c55
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-09-12 00:39:18 +0000

    MYPLAN-2347

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/plan/controller/PlanController.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/plan/controller/PlanController.java
@@ -1561,7 +1561,7 @@ public class PlanController extends UifControllerBase {
             note = hasText(note) ? String.format("'%s'", WordUtils.wrap(note.trim(), 80, "<br />", true)) : "";
             String dateAdded = DateFormatHelper.getDateFomatted(new Date(System.currentTimeMillis()).toString());
             String planLink = makeLinkToAtp(AtpHelper.termToYearTerm(term).toATP(), "visit your plan page");
-            courseCd = title != null ? String.format("%s '%s' placeHolder", courseCd, title) : courseCd;
+            courseCd = title != null ? (title.equals(courseCd) ? String.format("'%s' placeholder", courseCd) : String.format("%s '%s' placeholder", courseCd, title)) : courseCd;
 
             /*Creating a new Message from Adviser to student*/
             String subject = "";
