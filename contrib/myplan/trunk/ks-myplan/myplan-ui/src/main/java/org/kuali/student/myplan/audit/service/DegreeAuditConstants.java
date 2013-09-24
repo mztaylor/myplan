3f6ec0b67e72e79cf873a60b86ad44209fb334d0
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2013-09-24 22:15:29 +0000

    2390

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/audit/service/DegreeAuditConstants.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/audit/service/DegreeAuditConstants.java
@@ -52,7 +52,7 @@ public class DegreeAuditConstants extends DegreeAuditServiceConstants {
             "Audit processing failed due to '%s'" +
             "</li>" +
             "</ul>" +
-            "</div>";
+            "<input name=\"script\" type=\"hidden\" data-role=\"script\" value=\"removeCookie();\"/></div>";
     public static final String IS_AUDIT_SERVICE_UP = "isAuditServiceRunning";
 
     public static final String AUDIT_STATUS_ERROR_MSG = "audit status returned error";
