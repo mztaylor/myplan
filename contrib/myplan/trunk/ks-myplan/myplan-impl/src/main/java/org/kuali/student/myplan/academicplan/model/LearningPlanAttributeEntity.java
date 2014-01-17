39f30c63836c7a21bdae8431c2811c0e6158d8ee
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-01-17 21:10:16 +0000

    reservedTime implementation is fixed and also the saved schedule is fixed .  Overriding the Attribute value property column definition to be a CLOB instead of varchar2

--- contrib/myplan/trunk/ks-myplan/myplan-impl/src/main/java/org/kuali/student/myplan/academicplan/model/LearningPlanAttributeEntity.java
+++ contrib/myplan/trunk/ks-myplan/myplan-impl/src/main/java/org/kuali/student/myplan/academicplan/model/LearningPlanAttributeEntity.java
@@ -3,14 +3,18 @@ package org.kuali.student.myplan.academicplan.model;
 import org.kuali.student.r2.common.entity.BaseAttributeEntity;
 import org.kuali.student.r2.common.infc.Attribute;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "KSPL_LRNG_PLAN_ATTR")
+@AttributeOverride(name="value", column=@Column(name="ATTR_VALUE", columnDefinition = "CLOB"))
 public class LearningPlanAttributeEntity extends BaseAttributeEntity<LearningPlanEntity> {
 
-    public LearningPlanAttributeEntity() {}
+    public LearningPlanAttributeEntity() {
+    }
 
     public LearningPlanAttributeEntity(Attribute att, LearningPlanEntity owner) {
         super(att, owner);
