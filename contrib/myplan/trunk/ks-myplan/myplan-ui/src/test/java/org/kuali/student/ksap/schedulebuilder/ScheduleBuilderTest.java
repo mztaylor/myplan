f7581cc81fc4352203f2fb27556eaa9d08fac948
kmuthusw <kmuthusw@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-01-28 19:44:29 +0000

    Added dummy ScheduleBuilderTest

---
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/test/java/org/kuali/student/ksap/schedulebuilder/ScheduleBuilderTest.java
@@ -0,0 +1,21 @@
+package org.kuali.student.ksap.schedulebuilder;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * kmuthu Don't forget to add comment
+ *
+ * @Author kmuthu
+ * Date: 1/28/14
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ScheduleBuilderTest {
+
+    @Test
+    public void testScheduleOption1() {
+        Assert.assertTrue(1 == 2);
+    }
+}
