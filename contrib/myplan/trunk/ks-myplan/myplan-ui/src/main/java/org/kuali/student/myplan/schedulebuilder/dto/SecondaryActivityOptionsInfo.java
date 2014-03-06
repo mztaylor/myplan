e3fb026b6f66eefe104f03b04b04be030302b87a
dbmc <dbmc@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-03-06 00:52:09 +0000

    comment: change getSecondaryOptions, getActivityOptions to return copies of instance lists instead of unmodifiable lists. Part of schedule builder coalesce sections.

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/schedulebuilder/dto/SecondaryActivityOptionsInfo.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/schedulebuilder/dto/SecondaryActivityOptionsInfo.java
@@ -10,123 +10,125 @@ import java.util.List;
 import java.util.ArrayList;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "SecondaryActivityOptionsInfo", propOrder = { "index",
-		"uniqueId", "activityUniqueId", "activityTypeDescription", "enrollmentGroup",
-		"activityOptions", "_futureElements" })
+@XmlType(name = "SecondaryActivityOptionsInfo", propOrder = {"index",
+        "uniqueId", "activityUniqueId", "activityTypeDescription", "enrollmentGroup",
+        "activityOptions", "_futureElements"})
 public class SecondaryActivityOptionsInfo implements SecondaryActivityOptions,
-		Serializable {
+        Serializable {
 
-	private static final long serialVersionUID = -6831820824316403614L;
+    private static final long serialVersionUID = -6831820824316403614L;
 
-	@XmlAttribute
-	private int index;
+    @XmlAttribute
+    private int index;
 
-	@XmlAttribute
-	private String uniqueId;
+    @XmlAttribute
+    private String uniqueId;
 
-	@XmlAttribute
-	private String activityUniqueId;
+    @XmlAttribute
+    private String activityUniqueId;
 
-	@XmlAttribute
-	private String activityTypeDescription;
+    @XmlAttribute
+    private String activityTypeDescription;
 
-	@XmlAttribute
-	private boolean enrollmentGroup;
+    @XmlAttribute
+    private boolean enrollmentGroup;
 
-	@XmlElement
-	private List<ActivityOptionInfo> activityOptions;
+    @XmlElement
+    private List<ActivityOptionInfo> activityOptions;
 
-	@XmlAnyElement
-	private List<?> _futureElements;
+    @XmlAnyElement
+    private List<?> _futureElements;
 
-	public SecondaryActivityOptionsInfo() {
-	}
+    public SecondaryActivityOptionsInfo() {
+    }
 
-	public SecondaryActivityOptionsInfo(SecondaryActivityOptions copy) {
-		uniqueId = copy.getUniqueId();
-		activityUniqueId = copy.getActivityUniqueId();
-		activityTypeDescription = copy.getActivityTypeDescription();
-		enrollmentGroup = copy.isEnrollmentGroup();
-		setActivityOptions(copy.getActivityOptions());
-	}
+    public SecondaryActivityOptionsInfo(SecondaryActivityOptions copy) {
+        uniqueId = copy.getUniqueId();
+        activityUniqueId = copy.getActivityUniqueId();
+        activityTypeDescription = copy.getActivityTypeDescription();
+        enrollmentGroup = copy.isEnrollmentGroup();
+        setActivityOptions(copy.getActivityOptions());
+    }
 
-	@Override
-	public int getIndex() {
-		return index;
-	}
+    @Override
+    public int getIndex() {
+        return index;
+    }
 
-	public void setIndex(int index) {
-		this.index = index;
-	}
+    public void setIndex(int index) {
+        this.index = index;
+    }
 
-	@Override
-	public String getUniqueId() {
-		return uniqueId;
-	}
+    @Override
+    public String getUniqueId() {
+        return uniqueId;
+    }
 
-	public void setUniqueId(String uniqueId) {
-		this.uniqueId = uniqueId;
-	}
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
 
-	@Override
-	public String getActivityUniqueId() {
-		return activityUniqueId;
-	}
+    @Override
+    public String getActivityUniqueId() {
+        return activityUniqueId;
+    }
 
-	public void setActivityUniqueId(String activityUniqueId) {
-		this.activityUniqueId = activityUniqueId;
-	}
+    public void setActivityUniqueId(String activityUniqueId) {
+        this.activityUniqueId = activityUniqueId;
+    }
 
-	@Override
-	public String getActivityTypeDescription() {
-		return activityTypeDescription;
-	}
+    @Override
+    public String getActivityTypeDescription() {
+        return activityTypeDescription;
+    }
 
-	public void setActivityTypeDescription(String activityTypeDescription) {
-		this.activityTypeDescription = activityTypeDescription;
-	}
+    public void setActivityTypeDescription(String activityTypeDescription) {
+        this.activityTypeDescription = activityTypeDescription;
+    }
 
-	@Override
-	public int getActivityCount(boolean includeClosed) {
-		if (includeClosed)
-			return activityOptions == null ? 0 : activityOptions.size();
-		int c = 0;
-		if (activityOptions != null)
-			for (ActivityOption ao : activityOptions)
-				if (ao.isLockedIn() || includeClosed || !ao.isClosed())
-					c++;
-		return c;
-	}
+    @Override
+    public int getActivityCount(boolean includeClosed) {
+        if (includeClosed)
+            return activityOptions == null ? 0 : activityOptions.size();
+        int c = 0;
+        if (activityOptions != null)
+            for (ActivityOption ao : activityOptions)
+                if (ao.isLockedIn() || includeClosed || !ao.isClosed())
+                    c++;
+        return c;
+    }
 
-	@Override
-	public int getSelectedActivityCount(boolean includeClosed) {
-		int c = 0;
-		if (activityOptions != null)
-			for (ActivityOption ao : activityOptions)
-				if (ao.isLockedIn() || ((includeClosed || !ao.isClosed()) && ao.isSelected()))
-					c++;
-		return c;
-	}
+    @Override
+    public int getSelectedActivityCount(boolean includeClosed) {
+        int c = 0;
+        if (activityOptions != null)
+            for (ActivityOption ao : activityOptions)
+                if (ao.isLockedIn() || ((includeClosed || !ao.isClosed()) && ao.isSelected()))
+                    c++;
+        return c;
+    }
 
-	public boolean isEnrollmentGroup() {
-		return enrollmentGroup;
-	}
+    public boolean isEnrollmentGroup() {
+        return enrollmentGroup;
+    }
 
-	public void setEnrollmentGroup(boolean enrollmentGroup) {
-		this.enrollmentGroup = enrollmentGroup;
-	}
+    public void setEnrollmentGroup(boolean enrollmentGroup) {
+        this.enrollmentGroup = enrollmentGroup;
+    }
 
-	@Override
-	public List<ActivityOption> getActivityOptions() {
-		return activityOptions == null ? Collections
-				.<ActivityOption> emptyList() : Collections
-				.<ActivityOption> unmodifiableList(activityOptions);
-	}
+    @Override
+    public List<ActivityOption> getActivityOptions() {
+        List<ActivityOption> activityOptionList = new ArrayList<ActivityOption>();
+        if(activityOptions!=null){
+            activityOptionList.addAll(activityOptions);
+        }
+        return activityOptionList;
+    }
 
 
-	public List<ActivityOption> getSelectedSecondaryActivityOptions() {
-         if (activityOptions == null) {
-            return Collections.<ActivityOption> emptyList();
+    public List<ActivityOption> getSelectedSecondaryActivityOptions() {
+        if (activityOptions == null) {
+            return Collections.<ActivityOption>emptyList();
         }
 
         List<ActivityOption> secondaryActivities = new ArrayList<ActivityOption>();
@@ -137,29 +139,29 @@ public class SecondaryActivityOptionsInfo implements SecondaryActivityOptions,
         }
 
         return secondaryActivities;
-	}
+    }
 
 
-	public void setActivityOptions(
-			List<? extends ActivityOption> secondaryOptions) {
-		if (secondaryOptions != null) {
-			List<ActivityOptionInfo> meetingTimes = new java.util.ArrayList<ActivityOptionInfo>(
-					secondaryOptions.size());
-			for (ActivityOption secondaryOption : secondaryOptions) {
-				meetingTimes.add(new ActivityOptionInfo(secondaryOption));
-			}
-			this.activityOptions = meetingTimes;
-		} else {
-			this.activityOptions = null;
-		}
-	}
+    public void setActivityOptions(
+            List<? extends ActivityOption> secondaryOptions) {
+        if (secondaryOptions != null) {
+            List<ActivityOptionInfo> meetingTimes = new java.util.ArrayList<ActivityOptionInfo>(
+                    secondaryOptions.size());
+            for (ActivityOption secondaryOption : secondaryOptions) {
+                meetingTimes.add(new ActivityOptionInfo(secondaryOption));
+            }
+            this.activityOptions = meetingTimes;
+        } else {
+            this.activityOptions = null;
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "SecondaryActivityOptionsInfo [activityTypeDescription="
-				+ activityTypeDescription + ", enrollmentGroup="
-				+ enrollmentGroup + ", activityOptions=" + activityOptions
-				+ "]";
-	}
+    @Override
+    public String toString() {
+        return "SecondaryActivityOptionsInfo [activityTypeDescription="
+                + activityTypeDescription + ", enrollmentGroup="
+                + enrollmentGroup + ", activityOptions=" + activityOptions
+                + "]";
+    }
 
 }
