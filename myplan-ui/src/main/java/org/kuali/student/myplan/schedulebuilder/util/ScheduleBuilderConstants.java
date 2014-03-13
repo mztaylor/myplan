9b62506008618630a73f357720ee10025b6a3219
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-03-13 21:15:36 +0000

    added config param for possible schedules max count

--- myplan-ui/src/main/java/org/kuali/student/myplan/schedulebuilder/util/ScheduleBuilderConstants.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/schedulebuilder/util/ScheduleBuilderConstants.java
@@ -1,5 +1,8 @@
 package org.kuali.student.myplan.schedulebuilder.util;
 
+import org.kuali.rice.core.api.config.property.ConfigContext;
+import org.kuali.student.myplan.comment.CommentConstants;
+
 /**
  * Created by hemanthg on 2/13/14.
  */
@@ -10,4 +13,6 @@ public class ScheduleBuilderConstants {
     public static final String INVALID_SAVED_SCHEDULE = "myplan.text.error.scheduleBuilder.invalid.course";
     public static final String INVALID_SAVED_SCHEDULE_ACTIVITY = "myplan.text.error.scheduleBuilder.invalid.activities";
     public static final String SAVED_SCHEDULE_ACTIVITY_MOVED = "myplan.text.error.scheduleBuilder.moved.activity";
+
+    public static final int POSSIBLE_SCHEDULES_MAX_COUNT = ConfigContext.getCurrentContextConfig().getProperty("ks.possible.schedules.count") != null ? Integer.parseInt(ConfigContext.getCurrentContextConfig().getProperty("ks.possible.schedules.count")) : 1000;
 }
