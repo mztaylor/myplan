436676e1908d43d9de7ad5c4d7be9cdb12a3bf04
dbmc <dbmc@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-03-28 23:13:42 +0000

    add new AoListProcessor class and related implementations for refactoring some ScheduleBuilder code. None of this code is currently called anywhere except UwScheduleBuilderTest so it does not change the application.

---
+++ myplan-ui/src/main/java/org/kuali/student/myplan/schedulebuilder/support/AbstractAoListProcessor.java
@@ -0,0 +1,62 @@
+package org.kuali.student.myplan.schedulebuilder.support;
+
+import org.kuali.student.myplan.schedulebuilder.infc.AoListProcessor;
+
+
+/**
+ * Abstract implementaion of AoListProcessor that adds a few more methods and
+ * adds default implementation.
+ * Expect this to be extended by filter AoListProcessors especially if they
+ * want to have a count of AO that are mutated.
+ *
+ */
+public abstract class AbstractAoListProcessor implements AoListProcessor {
+    // count is provided to track the number of AO that are affected by the processor;
+    // e.g., for a filter processor, it counts the number of AO that are deleted.
+    // the processor can decide what is worth counting.  Use is optional.
+    private int count;
+
+    // return a code that identifies the processor, presumably defined elsewhere. Optional.
+    private int processorCode;
+
+    // return a string that describes the processor, presumably to bubble up to a UI somewhere
+    // so user can see something like "17 sections unavailable because they are closed". Optional.
+    private String processorDescription;
+
+    public void startCount() {
+        count = 0;
+    }
+
+    public int incCount() {
+        return ++count;
+    }
+
+     public int incCount(int addend) {
+        count += addend;
+        return count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getProcessorDescription() {
+        return processorDescription;
+    }
+
+    public void setProcessorDescription(String processorDescription) {
+        this.processorDescription = processorDescription;
+    }
+
+    public int getProcessorCode() {
+        return processorCode;
+    }
+
+    public void setProcessorCode(int processorCode) {
+        this.processorCode = processorCode;
+    }
+}
