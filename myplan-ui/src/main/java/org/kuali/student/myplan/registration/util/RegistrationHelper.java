518328a77d2020249bf3428ceaee560cdebb036a
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-04-28 16:37:40 +0000

    fixed the reg page loading css js issue

---
+++ myplan-ui/src/main/java/org/kuali/student/myplan/registration/util/RegistrationHelper.java
@@ -0,0 +1,14 @@
+package org.kuali.student.myplan.registration.util;
+
+/**
+ * Created by hemanthg on 4/25/2014.
+ */
+public interface RegistrationHelper {
+
+    /**
+     * Get the initial registration form.
+     *
+     * @return The initial registration form.
+     */
+    RegistrationForm getInitialForm();
+}
