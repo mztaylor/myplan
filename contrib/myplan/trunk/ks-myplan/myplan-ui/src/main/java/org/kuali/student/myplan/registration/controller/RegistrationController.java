6f4646974b40272eab7b69867c46ef35beae7f9a
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-04-30 17:30:45 +0000

    moved uw specific code to Uw overrides

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/registration/controller/RegistrationController.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/registration/controller/RegistrationController.java
@@ -3,38 +3,19 @@ package org.kuali.student.myplan.registration.controller;
 import org.kuali.rice.krad.web.controller.UifControllerBase;
 import org.kuali.rice.krad.web.form.UifFormBase;
 import org.kuali.student.myplan.config.UwMyplanServiceLocator;
-import org.kuali.student.myplan.registration.form.DefaultRegistrationForm;
-import org.kuali.student.myplan.registration.util.RegistrationForm;
 import org.kuali.student.myplan.registration.util.RegistrationHelper;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Created by hemanthg on 4/22/2014.
  */
-@Controller
-@RequestMapping(value = "/registration")
-public class RegistrationController extends UifControllerBase {
-
+public abstract class RegistrationController extends UifControllerBase {
     private RegistrationHelper registrationHelper;
 
     @Override
     protected UifFormBase createInitialForm(HttpServletRequest request) {
         return (UifFormBase) getRegistrationHelper().getInitialForm();
-    }
-
-    @RequestMapping(params = "methodToCall=registrationDetails")
-    public ModelAndView registrationDetails(@ModelAttribute("KualiForm") UifFormBase form, BindingResult result, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        RegistrationForm regForm = (RegistrationForm) form;
-        regForm.buildRegistrationDetails();
-        return getUIFModelAndView(form);
     }
 
     public RegistrationHelper getRegistrationHelper() {
@@ -47,4 +28,5 @@ public class RegistrationController extends UifControllerBase {
     public void setRegistrationHelper(RegistrationHelper registrationHelper) {
         this.registrationHelper = registrationHelper;
     }
+
 }
--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/registration/util/RegistrationForm.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/registration/util/RegistrationForm.java
@@ -67,8 +67,4 @@ public interface RegistrationForm {
      */
     String getRequestedLearningPlanId();
 
-    /**
-     * Builds the registration details.
-     */
-    void buildRegistrationDetails();
 }
