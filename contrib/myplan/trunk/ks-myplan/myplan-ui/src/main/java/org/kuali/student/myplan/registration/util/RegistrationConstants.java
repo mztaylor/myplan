3fe7c7464ab576f74e9a2fa826f5dc282728e1d2
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-05-08 18:05:16 +0000

    reworked on the open for registration flag implementation and added test cases

--- contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/registration/util/RegistrationConstants.java
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/registration/util/RegistrationConstants.java
@@ -14,8 +14,10 @@ public class RegistrationConstants {
 
     public static final String REGISTRATION_SERVER_PARAM = "myplan.registration.server";
     public static final String REGISTRATION_START_TIME_PARAM = "myplan.registration.start.time";
+    public static final String REGISTRATION_END_TIME_PARAM = "myplan.registration.end.time";
     public static final String REGISTRATION_URL_FORMAT = ConfigContext.getCurrentContextConfig().getProperty(REGISTRATION_SERVER_PARAM) + "/students/UWNetID/register.asp?QTR=%s&YR=%s&INPUTFORM=UPDATE&PAC=0&MAXDROPS=0&_CW=%s&%s";
     public static final String REGISTRATION_OPEN_TIME = ConfigContext.getCurrentContextConfig().getProperty(REGISTRATION_START_TIME_PARAM);
+    public static final String REGISTRATION_CLOSE_TIME = ConfigContext.getCurrentContextConfig().getProperty(REGISTRATION_END_TIME_PARAM);
     public static final String REGISTRATION_CODE_URL_PARAMS_FORMAT = "sln%s=%s&entcode%s=&credits%s=&gr_sys%s=";
 
     public static final String ERROR_COUNT_REGISTRATION_CODES = "myplan.text.error.check.registration.codes";
