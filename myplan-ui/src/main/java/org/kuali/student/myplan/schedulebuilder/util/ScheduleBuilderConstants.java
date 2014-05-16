5c974d50a2c8f0520b70127a7bb65cddbc52e2af
garettg <garettg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-05-16 18:44:07 +0000

    Myplan 2899-- Fix by Hemanth

--- myplan-ui/src/main/java/org/kuali/student/myplan/schedulebuilder/util/ScheduleBuilderConstants.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/schedulebuilder/util/ScheduleBuilderConstants.java
@@ -12,6 +12,8 @@ public class ScheduleBuilderConstants {
 
     public static final String SCHEDULE_BUILDER_PAGE_ID = "schedule_build";
 
+    public static final String POSSIBLE_SCHEDULE_COMPONENT_ID = "schedule_build";
+
     public static final String INVALID_PINNED_SCHEDULE = "myplan.text.error.scheduleBuilder.invalid.pinned.schedule";
 
     public static final String VALID_PINNED_SCHEDULE_WITHDRAWN_SINGLE = "myplan.text.error.scheduleBuilder.valid.pinned.schedule.withdrawn.single";
