698a8bbf89c60cbab8fdfaae3724c3bc5d9a794d
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-07-30 02:55:26 +0000

    Added this file as a new utility class to find if the user is authorized to view components.

---
+++ contrib/myplan/trunk/ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/utils/MyplanRoleUtils.java
@@ -0,0 +1,31 @@
+package org.kuali.student.myplan.utils;
+
+import org.kuali.rice.krad.uif.view.ViewAuthorizerBase;
+import org.kuali.rice.krad.util.KRADConstants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by hemanth on 7/29/14.
+ */
+public class MyplanRoleUtils extends ViewAuthorizerBase {
+
+
+    public static final String AUTHORIZED_TO_VIEW = "authorizedToView";
+
+    /**
+     * This Utility Method is used to define if the user is authorized to view the component or not.
+     *
+     * @param principalId
+     * @param expectedRoles
+     * @return
+     */
+    public static boolean principalHasRole(String principalId, String expectedRoles) {
+        Map<String, String> roleQualifiers = new HashMap<String, String>();
+        roleQualifiers.put(AUTHORIZED_TO_VIEW, expectedRoles);
+        boolean isAuthorized = getPermissionService().isAuthorizedByTemplate(principalId, KRADConstants.KRAD_NAMESPACE, "View", new HashMap<String, String>(), roleQualifiers);
+        return isAuthorized;
+    }
+
+}
