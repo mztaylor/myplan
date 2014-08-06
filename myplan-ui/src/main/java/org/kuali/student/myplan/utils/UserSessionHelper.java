560053dcb15dd06c783fca7535f1fda6704a2ad5
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-08-06 18:16:07 +0000

    Cahing the calls made to rice permission service

--- myplan-ui/src/main/java/org/kuali/student/myplan/utils/UserSessionHelper.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/utils/UserSessionHelper.java
@@ -1,11 +1,19 @@
 package org.kuali.student.myplan.utils;
 
+import org.kuali.student.myplan.audit.service.DegreeAuditServiceConstants;
 import org.kuali.student.r2.common.dto.ContextInfo;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
+
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import java.util.Map;
 
 /**
  * Provides an initialized Context which can be used for service requests.
  */
+@WebService(name = "UserSessionHelper", serviceName = "UserSessionHelper", portName = "UserSessionHelper", targetNamespace = "http://student.kuali.org/wsdl/userSession")
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public interface UserSessionHelper {
 
     public ContextInfo makeContextInfoInstance();
@@ -37,5 +45,9 @@ public interface UserSessionHelper {
     public String getStudentExternalIdentifier();
 
     public String getExternalIdentifier(String regId);
+
+    public boolean authorizedByTemplate(String userId, Map<String, String> roleQualifiers, String permissionTemplateName);
+
+
 
 }
