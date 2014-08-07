78681349b3a4b2b52ffd301864678ba96ddb8bb6
hemanthg <hemanthg@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2014-08-07 18:51:07 +0000

    changed class name to be Ksaproleutils instead of myplanroleutils

--- myplan-ui/src/main/java/org/kuali/student/myplan/view/MyplanLookupViewAuthorizer.java
+++ myplan-ui/src/main/java/org/kuali/student/myplan/view/MyplanLookupViewAuthorizer.java
@@ -7,18 +7,8 @@ import org.kuali.rice.krad.uif.field.Field;
 import org.kuali.rice.krad.uif.view.View;
 import org.kuali.rice.krad.uif.view.ViewModel;
 import org.kuali.rice.krad.uif.widget.Widget;
-import org.kuali.rice.krad.util.KRADConstants;
-import org.kuali.student.myplan.config.UwMyplanServiceLocator;
-import org.kuali.student.myplan.plan.PlanConstants;
 import org.kuali.student.myplan.utils.GlobalConstants;
-import org.kuali.student.myplan.utils.MyplanRoleUtils;
-import org.kuali.student.myplan.utils.UserSessionHelper;
-import org.kuali.student.r2.common.dto.ContextInfo;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.kuali.student.myplan.utils.KSAPRoleUtils;
 
 /**
  * Created by hemanthg on 6/26/2014.
@@ -28,24 +18,24 @@ public class MyplanLookupViewAuthorizer extends LookupViewAuthorizerBase {
 
     @Override
     public boolean canOpenView(View view, ViewModel model, Person user) {
-        return MyplanRoleUtils.principalHasRole(user.getPrincipalId(),view.getComponentSecurity().getAdditionalRoleQualifiers().get(GlobalConstants.AUTHORIZED_TO_VIEW));
+        return KSAPRoleUtils.principalHasRole(user.getPrincipalId(), view.getComponentSecurity().getAdditionalRoleQualifiers().get(GlobalConstants.AUTHORIZED_TO_VIEW));
     }
 
 
     @Override
     public boolean canViewGroup(View view, ViewModel model, Group group, String groupId, Person user) {
-        return MyplanRoleUtils.principalHasRole(user.getPrincipalId(),group.getComponentSecurity().getAdditionalRoleQualifiers().get(GlobalConstants.AUTHORIZED_TO_VIEW));
+        return KSAPRoleUtils.principalHasRole(user.getPrincipalId(), group.getComponentSecurity().getAdditionalRoleQualifiers().get(GlobalConstants.AUTHORIZED_TO_VIEW));
     }
 
 
     @Override
     public boolean canViewField(View view, ViewModel model, Field field, String propertyName, Person user) {
-        return MyplanRoleUtils.principalHasRole(user.getPrincipalId(),field.getComponentSecurity().getAdditionalRoleQualifiers().get(GlobalConstants.AUTHORIZED_TO_VIEW));
+        return KSAPRoleUtils.principalHasRole(user.getPrincipalId(), field.getComponentSecurity().getAdditionalRoleQualifiers().get(GlobalConstants.AUTHORIZED_TO_VIEW));
     }
 
     @Override
     public boolean canViewWidget(View view, ViewModel model, Widget widget, String widgetId, Person user) {
-        return MyplanRoleUtils.principalHasRole(user.getPrincipalId(),widget.getComponentSecurity().getAdditionalRoleQualifiers().get(GlobalConstants.AUTHORIZED_TO_VIEW));
+        return KSAPRoleUtils.principalHasRole(user.getPrincipalId(), widget.getComponentSecurity().getAdditionalRoleQualifiers().get(GlobalConstants.AUTHORIZED_TO_VIEW));
     }
 
 
